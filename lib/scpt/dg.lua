0eb9557f5a936fb6b4e5f8eae28eb16445bc9226
darkgod <darkgod>
2001-11-19 16:03:06 +0000

    lua

---
+++ lib/scpt/dg.lua
@@ -0,0 +1,1 @@
+pern_dofile(0, "test.lua")
--- lib/scpt/init.lua
+++ lib/scpt/init.lua
@@ -1,3 +1,6 @@
 -- Put whatever is needed here
 
-pern_dofile(Ind, "test.lua")
+pern_dofile(Ind, "dg.lua")
+pern_dofile(Ind, "evil.lua")
+pern_dofile(Ind, "zz.lua")
+pern_dofile(Ind, "jir.lua")
