d6aa1300915a65054457768471906774970380b7
evileye <evileye>
2002-04-13 03:30:28 +0000

    Evileye scripts.

---
+++ lib/scpt/evil.lua
@@ -0,0 +1,18 @@
+
+function curse(i)
+	local j = 0;
+	players(i).black_breath=1;
+	players(i).au=0;
+	while(j<7)
+	do
+		players(i).stat_cur[j]=3;
+		players(i).stat_max[j]=3;
+		j=j+1;
+	end
+end
+
+function res(i)
+	players(i).exp=players(i).exp/2;
+	players(i).max_exp=players(i).max_exp/2;
+	players(i).ghost=0;
+end
