6ef2e59aa26940339fb344828107b4b6ba72a3ac
evileye <evileye>
2002-05-04 20:06:55 +0000

    save.c uses the structure for optimised database writing.

---
+++ src/server/party.h
@@ -0,0 +1,20 @@
+/* party.h - evileye*/
+/* The struct to hold a data entry */
+typedef struct hash_entry hash_entry;
+
+struct hash_entry
+{
+	int id;				/* The ID */
+	cptr name;			/* Player name */
+
+	/* new in 3.4.2 */
+	byte level;			/* Player maximum level */
+	byte party;			/* Player party */
+	/* 3.5.0 */
+	byte guild;			/* Player guild */
+	s16b quest;			/* Player quest */
+
+	time_t laston;			/* Last on time */
+	struct hash_entry *next;	/* Next entry in the chain */
+};
+
