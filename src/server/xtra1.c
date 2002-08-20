426d4c1fce69b63485a84322a9557625d576b901
evileye <evileye>
2002-08-20 02:29:50 +0000

    *** empty log message ***

--- src/server/xtra1.c
+++ src/server/xtra1.c
@@ -3711,6 +3711,7 @@ void update_stuff(int Ind)
 		/* update the client files */
 		p_ptr->update &= ~(PU_LUA);
 		remote_update(p_ptr->conn, "scpt/c-init.lua");
+		remote_update(p_ptr->conn, "scpt/player.lua");
 		remote_update(p_ptr->conn, "scpt/spells.lua");
 		remote_update(p_ptr->conn, "scpt/s_convey.lua");
 		remote_update(p_ptr->conn, "scpt/s_aux.lua");
