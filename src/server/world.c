726400c348d6e81a803e64494deb970875761190
evileye <evileye>
2002-09-03 15:12:36 +0000

    No more crypt

--- src/server/world.c
+++ src/server/world.c
@@ -41,12 +41,15 @@ int world_comm(int fd, int arg){
 				break;
 			case WP_AUTH:
 				/* Authentication request */
-				strcpy(wpk->d.auth.pass, crypt(cfg.pass, wpk->d.auth.pass));
+				wpk->d.auth.val=chk(cfg.pass, wpk->d.auth.pass);
 				x=sizeof(struct wpacket);
 				x=send(WorldSocket, wpk, x, 0);
 				break;
+			case WP_SQUIT:
+				/* Remove players */
+				rem_players(wpk->d.sid);
 			default:
-				printf("unknown packet from world\n");
+				s_printf("unknown packet from world: %d\n", wpk->type);
 		}
 	}
 	if(x==0){
@@ -72,11 +75,32 @@ void world_remote_players(FILE *fff){
 	}
 }
 
+void rem_players(short id){
+	struct rplist *c_pl, *p_pl, *d_pl;
+	c_pl=rpmlist;
+	p_pl=c_pl;
+	while(c_pl){
+		d_pl=NULL;
+		if(c_pl->server==id){
+			if(c_pl==rpmlist)
+				rpmlist=c_pl->next;
+			else
+				p_pl->next=c_pl->next;
+			d_pl=c_pl;
+		}
+		p_pl=c_pl;
+		c_pl=c_pl->next;
+		if(d_pl) free(d_pl);
+	}
+}
+
 void add_rplayer(struct wpacket *wpk){
 	struct rplist *n_pl, *c_pl;
 	unsigned short found=0;
 	if(!wpk->d.play.silent)
 		msg_broadcast_format(0, "\377s%s has %s the game.", wpk->d.play.name, (wpk->type==WP_NPLAYER ? "entered" : "left"));
+
+	if(wpk->type==WP_NPLAYER && !wpk->d.play.server) return;
 	c_pl=rpmlist;
 	while(c_pl){
 		if(c_pl->id==wpk->d.play.id && !(strcmp(c_pl->name, wpk->d.play.name))){
@@ -94,7 +118,7 @@ void add_rplayer(struct wpacket *wpk){
 		strncpy(n_pl->name, wpk->d.play.name, 30);
 		rpmlist=n_pl;
 	}
-	else if (found){
+	else if(wpk->type==WP_QPLAYER && found){
 		if(c_pl==rpmlist)
 			rpmlist=c_pl->next;
 		else
@@ -134,4 +158,28 @@ void world_player(unsigned long id, char *name, unsigned short enter, byte quiet
 	x=send(WorldSocket, &spk, len, 0);
 }
 
+/* unified, hopefully unique password check function */
+long chk(char *s1, char *s2){
+	int i, j=0;
+	int m1, m2;
+	static long rval[2]={0, 0};
+	m1=strlen(s1);
+	m2=strlen(s2);
+	for(i=0; i<m1; i++){
+		rval[0]+=s1[i];
+		rval[0]<<=5;
+	}
+	for(j=0; j<m2; j++){
+		rval[1]+=s2[j];
+		rval[1]<<=3;
+	}
+	for(i=0; i<m1; i++){
+		rval[1]+=s1[i];
+		rval[1]<<=(3+rval[0]%5);
+		rval[0]+=s2[j];
+		rval[0]<<=(3+rval[1]%3);
+		j=rval[0]%m2;
+	}
+	return(rval[0]+rval[1]);
+}
 #endif
