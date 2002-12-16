d323c2f218ad61e17575c8993448a2feea8e33de
evileye <evileye>
2002-12-16 04:56:34 +0000

    say_as fixed.. NB, lua uses DECIMAL, not octal.

--- lib/scpt/dg.lua
+++ lib/scpt/dg.lua
@@ -2,5 +2,5 @@ pern_dofile(0, "test.lua")
 
 -- Speak for someone else :)
 function say_as(who, what)
-        msg_broadcast(0, "{B["..who.."] "..what)
+        msg_broadcast(0, "\255g[\255o"..who.."\255g] \255B"..what)
 end
