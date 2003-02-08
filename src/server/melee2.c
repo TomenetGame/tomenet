063ce8a3ad324431714d0814cd27c209665101c1
cblue <cblue>
2003-02-08 22:24:24 +0000

    More restrictive LOS for summon/breath/ball

--- src/server/melee2.c
+++ src/server/melee2.c
@@ -69,7 +69,7 @@
 #define		INDIRECT_FREQ	50
  
 /* pseudo 'radius' for summoning spells. default is 3.  */
-#define		INDIRECT_SUMMONING_RADIUS	3
+#define		INDIRECT_SUMMONING_RADIUS	2
 
 /* if defined, a monster will simply choose the spell by RNG and
  * never hesitate to shoot its friends.
@@ -1559,7 +1559,7 @@ bool make_attack_spell(int Ind, int m_idx)
 
 	/* radius of ball spells and breathes.
 	 * XXX this doesn't reflect some exceptions(eg. radius=4 spells). */
-	srad = (r_ptr->flags2 & (RF2_POWERFUL)) ? 3 : 2;
+	srad = (r_ptr->flags2 & (RF2_POWERFUL)) ? 2 : 1; /* was 3 : 2 */
 
 	/* NOTE: it is abusable that MAX_RANGE is 18 and player arrow range
 	 * is 25-50; one can massacre uniques without any dangers.
