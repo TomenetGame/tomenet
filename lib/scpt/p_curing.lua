5a1d01f8b37876a9cdb02c859d88517a4d1d49ea
cblue <cblue>
2006-09-04 11:26:41 +0000

    healing fix

--- lib/scpt/p_curing.lua
+++ lib/scpt/p_curing.lua
@@ -3,8 +3,8 @@
 function get_healing_power2()
         local pow
         pow = player.mhp * (25 + get_level(Ind, HHEALING, 31)) / 100
-	if pow > get_level(Ind, HHEALING, 400)
-		pow = get_level(Ind, HHEALING, 400)
+	if pow > get_level(Ind, HHEALING, 417) then
+		pow = get_level(Ind, HHEALING, 417)
 	end
         if pow > 400 then
                 pow = 400
