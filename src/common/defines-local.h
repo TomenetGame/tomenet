f9a6e046fcaf60a777f9d265885bf696d1de54ee
cblue <cblue>
2007-12-28 16:51:46 +0000

    COMPLETE UPDATE 2007-12

---
+++ src/common/defines-local.h
@@ -0,0 +1,20 @@
+/* Purpose: Define server type. Included exclusively by "defines.h". */
+
+
+/* If the server is a normal TomeNET server, simply leave all of the
+   special server rules below commented out. - C. Blue */
+
+
+/* Server is a true RPG server? - C. Blue - This means..
+ * - 1 real-life person may only create up to 1 character in total
+ * - all characters are no-ghost by default (death is permanent)
+ * - dungeons are IRONMAN
+ * - and many more things that are just a little bit different :)
+ */
+/* #define RPG_SERVER */
+
+/* Server is Moltor's Smash Arcade server? */
+/* #define ARCADE_SERVER */
+
+/* Server running in 'Fun Mode'? Allows cheezing and cheating and everything. */
+/* #define FUN_SERVER */
