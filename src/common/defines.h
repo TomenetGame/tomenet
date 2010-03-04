f4ac46e8e90d52cb9f0541a97bc170979e5e1e8d
cblue <cblue>
2010-03-04 05:06:40 +0000

    unbumping savefile version

--- src/common/defines.h
+++ src/common/defines.h
@@ -85,8 +85,8 @@
 
 /* For savefile purpose only */
 #define SF_VERSION_MAJOR	4
-#define SF_VERSION_MINOR	4
-#define SF_VERSION_PATCH	1
+#define SF_VERSION_MINOR	3
+#define SF_VERSION_PATCH	26
 #define SF_VERSION_EXTRA	0
 
 
