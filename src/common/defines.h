fa69c0a320affba5e202964e97cc37559d9f1ec8
mikaelh <mikaelh>
2010-07-09 18:12:37 +0000

    TomeNET version 4.4.5g.

--- src/common/defines.h
+++ src/common/defines.h
@@ -54,7 +54,7 @@
 #define VERSION_MAJOR		4
 #define VERSION_MINOR		4
 #define VERSION_PATCH		5
-#define VERSION_EXTRA		8
+#define VERSION_EXTRA		9
 #define VERSION_BRANCH		0
 #define VERSION_BUILD		0
 
@@ -69,7 +69,7 @@
 #define VERSION_MAJOR_LATEST	4
 #define VERSION_MINOR_LATEST	4
 #define VERSION_PATCH_LATEST	5
-#define VERSION_EXTRA_LATEST	7
+#define VERSION_EXTRA_LATEST	8
 #define VERSION_BRANCH_LATEST	0
 #define VERSION_BUILD_LATEST	0
 
@@ -100,7 +100,7 @@
 
 /* Client-side only: Client release version tag
    (such as "a", "b" etc) used in window title and file dumps */
-#define CLIENT_VERSION_TAG "f"
+#define CLIENT_VERSION_TAG "g"
 
 
 
