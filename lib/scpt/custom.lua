05f7c0fb0c68e9d6018317a0fe2d7866534ea13a
cblue <cblue>
2010-11-29 12:10:55 +0000

    Server 4.4.6 release

--- lib/scpt/custom.lua
+++ lib/scpt/custom.lua
@@ -1,42 +1,65 @@
 -- C. Blue's automatic stuff --
 -------------------------------
 
+-- Special features:
+	--reset true artifacts every n months (must be one of 1,2,3,4,6)
+	art_reset_freq = 6
+
+-- Helper variables
+	cur_month = -1
+	cur_day = -1
+	cur_weekday = -1
+	cur_year = -1
+
 -- Run automatically on server starting up:
 function server_startup(timestamp, h, m, s, dwd, dd, dm, dy)
 	lua_s_print(timestamp.."_SERVERSTARTUP_"..h..":"..m..":"..s.."-"..dy.."/"..dm.."/"..dd.."("..dwd..")\n")
 
-        --[[automatically fix season if required, for example if server 
-            was down for 24+ hours while a season change date occured. 
-            see cron_24h() for details and to keep values synchronized.]]
-        if (season ~= 0) then
-                if ((dm == 3 and dd >= 20) or (dm > 3 and dm < 6) or (dm == 6 and dd < 21)) then
-                        lua_season_change(0, 0)
-                end
-        end
-        if (season ~= 1) then
-                if ((dm == 6 and dd >= 21) or (dm > 6 and dm < 9) or (dm == 9 and dd < 23)) then
-                        lua_season_change(1, 0)
-                end
-        end
-        if (season ~= 2) then
-                if ((dm == 9 and dd >= 23) or (dm > 9 and dm < 12) or (dm == 12 and dd < 21)) then
-                        lua_season_change(2, 0)
-                end
-        end
-        if (season ~= 3) then
-                if ((dm == 12 and dd >= 21) or (dm < 3) or (dm == 3 and dd < 20)) then
-                        lua_season_change(3, 0)
-                end
-        end
-
---	lua_add_anote("This is how you add server notes.");
+	cur_month = dm
+	cur_day = dd
+	cur_weekday = dwd
+	cur_year = dy
 
 	-- Admin parameters */
 	watch_nr = 1
 	watch_morgoth = 1
 
+	-- Server changes like rearranging arrays. Should NOT be changed here!
+--	updated_server = 0
+
 	-- Modify 'updated_savegame' value for newly created chars [0]
 	updated_savegame_birth = 0
+end
+
+-- Run automatically on play_game()/sched() starting up:
+function playloop_startup(timestamp, h, m, s, dwd, dd, dm, dy)
+	lua_s_print(timestamp.."_PLAYLOOPSTARTUP_"..h..":"..m..":"..s.."-"..dy.."/"..dm.."/"..dd.."("..dwd..")\n")
+
+        --[[automatically fix season if required, for example if server 
+            was down for 24+ hours while a season change date occured. 
+            see cron_24h() for details and to keep values synchronized.]]
+        if (season ~= 0) then
+                if (dm >= 3 and dm <= 5) then
+                        lua_season_change(0, 0)
+                end
+        end
+        if (season ~= 1) then
+                if ((dm >= 6 and dm <= 8) or (dm == 9 and dd < 23)) then
+                        lua_season_change(1, 0)
+                end
+        end
+        if (season ~= 2) then
+                if ((dm == 9 and dd >= 23) or (dm >= 10 and dm <= 11) or (dm == 12 and dd < 22)) then
+                        lua_season_change(2, 0)
+                end
+        end
+        if (season ~= 3) then
+                if ((dm == 12 and dd >= 22) or (dm < 3)) then
+                        lua_season_change(3, 0)
+                end
+        end
+
+--	lua_add_anote("This is how you add server notes.");
 end
 
 -- Run additionally when 1st player joins since starting up: (character fully loaded at this point)
@@ -109,8 +132,19 @@ function cron_24h(timestamp, h, m, s, dwd, dd, dm, dy)
 	--[[season changes (averaged, may in reality vary +/- 1 day:
 	    spring 20.march, summer 21.june,
 	    autumn 23. september, winter 21. december ]]
-	if (dd == 20 and dm == 3) then lua_season_change(0, 0) end
-	if (dd == 21 and dm == 6) then lua_season_change(1, 0) end
+	if (dd == 1 and dm == 3) then lua_season_change(0, 0) end
+	if (dd == 1 and dm == 6) then lua_season_change(1, 0) end
 	if (dd == 23 and dm == 9) then lua_season_change(2, 0) end
-	if (dd == 21 and dm == 12) then lua_season_change(3, 0) end
+	if (dd == 22 and dm == 12) then lua_season_change(3, 0) end
+
+	--regrow fields (gardens) sometimes :/
+	if mod(dd, 4) == rand_int(4) then
+		lively_wild(8192)
+	end
+
+	--Acknowledge +1 day progress of time
+	cur_month = dm
+	cur_day = dd
+	cur_weekday = dwd
+	cur_year = dy
 end
