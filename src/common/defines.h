fe0d55ef5ffd750c30290c86bf23e45c3875b85d
cblue <cblue>
2010-12-08 16:03:17 +0000

    bumped client to 4.4.6a for targetdir change of . to -

--- src/common/defines.h
+++ src/common/defines.h
@@ -54,7 +54,7 @@
 #define VERSION_MAJOR		4
 #define VERSION_MINOR		4
 #define VERSION_PATCH		6
-#define VERSION_EXTRA		0
+#define VERSION_EXTRA		1
 #define VERSION_BRANCH		0
 #define VERSION_BUILD		0
 
@@ -100,7 +100,7 @@
 
 /* Client-side only: Client release version tag
    (such as "a", "b" etc) used in window title and file dumps */
-#define CLIENT_VERSION_TAG ""
+#define CLIENT_VERSION_TAG "a"
 
 
 
