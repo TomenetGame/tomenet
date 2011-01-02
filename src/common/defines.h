e28f30f17f8e0b509ac546821a5fd92c723a46ed
cblue <cblue>
2011-01-02 08:57:57 +0000

    bump client to 4.4.6b

--- src/common/defines.h
+++ src/common/defines.h
@@ -100,7 +100,7 @@
 
 /* Client-side only: Client release version tag
    (such as "a", "b" etc) used in window title and file dumps */
-#define CLIENT_VERSION_TAG "a"
+#define CLIENT_VERSION_TAG "b"
 
 
 
