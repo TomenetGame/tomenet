adfa1e4af116ce5c260572f8c6fc08aef9cab4ba
cblue <cblue>
2011-02-28 13:08:07 +0000

    addd assassinate technique

--- src/client/c-tables.c
+++ src/client/c-tables.c
@@ -969,13 +969,13 @@ cptr melee_techniques[16] =
 #else
 
   "Bash",
-  "Knock-back",
+  "Knock-Back",
   "Charge",
   "Flash bomb",
 
   "Cloak",
   "Spin",
-  "",
+  "Assassinate",
   "Berserk",
 
   "",
