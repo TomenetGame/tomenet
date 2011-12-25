1f12d27f3a6120a285a73a459a4c28c4c0bc1f06
C. Blue <mail@c-blue.de>
2011-12-25 13:04:03 +0100

    bumped client version to 4.4.8b

--- src/common/defines.h
+++ src/common/defines.h
@@ -68,7 +68,7 @@
 
 /* Client-side only: Client release version tag
    (such as "a", "b" etc) used in window title and file dumps */
-#define CLIENT_VERSION_TAG "a"
+#define CLIENT_VERSION_TAG "b"
 
 /* Minimum client version required to be allowed to log in */
 #define MIN_VERSION_MAJOR	4
