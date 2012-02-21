514b1d89f8cee22782c98cea32b8bfebb2af4366
C. Blue <mail@c-blue.de>
2012-02-21 18:26:50 +0100

    removed obsolete 'You walk through the door' message, showing up even if you infact bump into the door

--- src/server/csfunc.c
+++ src/server/csfunc.c
@@ -104,14 +104,11 @@ void dnasave(c_special *cs_ptr){
 }
 int dnahit(c_special *cs_ptr, int y, int x, int Ind){
 	/* we have to know from where we are called! */
-	struct dna_type *dna=cs_ptr->sc.ptr;
+	struct dna_type *dna = cs_ptr->sc.ptr;
 	{
 		if (access_door(Ind, dna, TRUE) || admin_p(Ind)) {
 			if (admin_p(Ind))
 				msg_format(Ind, "\377gThis house is owned by %s.", get_house_owner(cs_ptr));
-#ifdef USE_MANG_HOUSE
-			msg_print(Ind, "\377GYou walk through the door.");
-#endif	/*USE_MANG_HOUSE */
 			return(TRUE);
 		}
 	}
