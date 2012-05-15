f4f54219bbd6fde6ba90f2f5adf050d62858b0f0
C. Blue <mail@c-blue.de>
2012-05-15 11:57:47 +0200

    mention poly-by-name in the macro wizard too

--- src/client/c-util.c
+++ src/client/c-util.c
@@ -3969,11 +3969,11 @@ void interact_macros(void)
 						Term_putstr(15, 17, -1, TERM_L_GREEN, "Enter exact technique name:");
 						break;
 					case 'h':
-						Term_putstr(10, 11, -1, TERM_GREEN, "Please enter the exact monster code. You can find codes");
-						Term_putstr(10, 12, -1, TERM_GREEN, "you have already learned by pressing  \377s~ 3  \377gin the game");
+						Term_putstr(10, 11, -1, TERM_GREEN, "Please enter the exact monster name OR its code. (You can find");
+						Term_putstr(10, 12, -1, TERM_GREEN, "codes you have already learned by pressing  \377s~ 3  \377gin the game");
 						Term_putstr(10, 13, -1, TERM_GREEN, "or by pressing  \377s:  \377gto chat and then typing the command:  \377s/mon");
-						Term_putstr(10, 14, -1, TERM_GREEN, "The first number on the left, in parentheses, is what you need.");
-						Term_putstr(10, 15, -1, TERM_GREEN, "For example, enter   \377G37   \377gto polymorph into a 'fruit bat'.");
+						Term_putstr(10, 14, -1, TERM_GREEN, "The first number on the left, in parentheses, is what you need.)");
+						Term_putstr(10, 15, -1, TERM_GREEN, "For example, enter  \377GFruit bat\377g  or just  \377G37  \377gto transform into one.");
 						Term_putstr(10, 16, -1, TERM_GREEN, "You must have learned a form before you can use it!");
 						Term_putstr(15, 18, -1, TERM_L_GREEN, "Enter exact monster code:");
 						break;
