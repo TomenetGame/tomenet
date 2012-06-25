0ff8b6c2bfdea4e0391b159e393ec7522ab5053a
C. Blue <mail@c-blue.de>
2012-06-25 02:32:29 +0200

    working on /backup_acclists and /restore_acclists

--- src/server/party.c
+++ src/server/party.c
@@ -3679,13 +3679,13 @@ void restore_acclists(void) {
 	while (!feof(fp)) {
 #ifdef AUCTION_SYSTEM
 		fscanf(fp, "\"%s\"\n%lu%d%u%d%d%d%d%d%d%d%lu%lu",
-		    ptr->name, &ptr->laston, &ptr->id, &ptr->account,
+		    name_forge, &ptr->laston, &ptr->id, &ptr->account,
 		    &ptr->level, &ptr->party, &ptr->guild,
 		    &ptr->quest, &ptr->race, &ptr->class, &ptr->mode,
 		    &ptr->au, &ptr->balance);
 #else
 		fscanf(fp, "\"%s\"\n%lu%d%u%d%d%d%d%d%d%d",
-		    ptr->name, &ptr->laston, &ptr->id, &ptr->account,
+		    name_forge, &ptr->laston, &ptr->id, &ptr->account,
 		    &ptr->level, &ptr->party, &ptr->guild,
 		    &ptr->quest, &ptr->race, &ptr->class, &ptr->mode);
 #endif
