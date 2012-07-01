41adcfca6b6c9186770e94c7f2283a9b24335055
C. Blue <mail@c-blue.de>
2012-07-01 18:05:07 +0200

    disabled tele sfx on using void jump gates

--- src/server/cmd2.c
+++ src/server/cmd2.c
@@ -405,7 +405,7 @@ static bool between_effect(int Ind, cave_type *c_ptr)
 	}
 
 #ifdef USE_SOUND_2010
-	sound(Ind, "teleport", NULL, SFX_TYPE_COMMAND, TRUE);
+//	sound(Ind, "teleport", NULL, SFX_TYPE_COMMAND, TRUE);
 #endif
 
 	swap_position(Ind, by, bx);
