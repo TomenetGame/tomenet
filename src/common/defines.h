9fff6ac664bd07060afbc8f408907a8a1fcbd3c6
C. Blue <mail@c-blue.de>
2012-07-08 19:45:02 +0200

    bump version for test client

--- src/common/defines.h
+++ src/common/defines.h
@@ -51,7 +51,7 @@
 #define VERSION_PATCH_LATEST	9
 #define VERSION_EXTRA_LATEST	0
 #define VERSION_BRANCH_LATEST	0
-#define VERSION_BUILD_LATEST	0
+#define VERSION_BUILD_LATEST	1
 
 /* maximum MAJOR/MINOR/PATCH version that counts as 'outdated' (should be 0-15). */
 #define VERSION_MAJOR_OUTDATED	4
