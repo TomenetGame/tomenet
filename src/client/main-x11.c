39767fd69a0e3f3a2e5965e5c3af2bf517dae515
C. Blue <mail@c-blue.de>
2012-07-12 10:32:37 +0200

    fix mem leaks

--- src/client/main-x11.c
+++ src/client/main-x11.c
@@ -1329,6 +1329,7 @@ static errr Infowin_set_size(int w, int h, int r_w, int r_h, bool fixed)
 	XSetWMNormalHints(Metadpy->dpy, Infowin->win, sh);
 
 	/* Success */
+	free(sh);
 	return 0;
 }
 
@@ -1355,6 +1356,7 @@ static errr Infowin_set_class_hint(cptr name)
 
 	XSetClassHint(Metadpy->dpy, Infowin->win, ch);
 
+	free(ch);
 	return (0);
 }
 
