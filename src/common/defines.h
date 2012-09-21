c7cc6a65d9a035c6d591c582685d636c69ab6fa3
C. Blue <mail@c-blue.de>
2012-09-21 16:25:26 +0200

    changed 'latest' to 4.4.9.2.0.0 again

--- src/common/defines.h
+++ src/common/defines.h
@@ -54,7 +54,7 @@
 #define VERSION_PATCH_LATEST	9
 #define VERSION_EXTRA_LATEST	2
 #define VERSION_BRANCH_LATEST	0
-#define VERSION_BUILD_LATEST	1
+#define VERSION_BUILD_LATEST	0
 
 /* maximum MAJOR/MINOR/PATCH version that counts as 'outdated' (should be 0-15). */
 #define VERSION_MAJOR_OUTDATED	4
