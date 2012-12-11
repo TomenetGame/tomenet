c0094a233856b81646e2ebe60b04a313b9e20bd3
C. Blue <mail@c-blue.de>
2012-12-11 06:41:46 +0100

    verbose msg about mana cost when trying a mimic spell

--- src/server/cmd5.c
+++ src/server/cmd5.c
@@ -463,7 +463,7 @@ static void do_mimic_power(int Ind, int power, int dir)
 
 	/* Check mana */
 	if (s_ptr->smana > p_ptr->csp) {
-		msg_print(Ind, "You do not have enough mana.");
+		msg_format(Ind, "You need %d mana to cast this spell.", s_ptr->smana);
 		return;
 	}
 
