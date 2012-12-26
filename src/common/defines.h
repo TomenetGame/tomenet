189b1c0f4082353e5fa14584e60778cfe4ab665f
C. Blue <mail@c-blue.de>
2012-12-26 17:48:08 +0100

    bumped version to 4.5.1a for macro wiz bugfix

--- src/common/defines.h
+++ src/common/defines.h
@@ -52,7 +52,7 @@
 #define VERSION_MAJOR_LATEST	4
 #define VERSION_MINOR_LATEST	5
 #define VERSION_PATCH_LATEST	1
-#define VERSION_EXTRA_LATEST	0
+#define VERSION_EXTRA_LATEST	1
 #define VERSION_BRANCH_LATEST	0
 #define VERSION_BUILD_LATEST	0
 
@@ -71,7 +71,7 @@
 
 /* Client-side only: Client release version tag
    (such as "a", "b" etc) used in window title and file dumps */
-#define CLIENT_VERSION_TAG	""
+#define CLIENT_VERSION_TAG	"a"
 
 /* Minimum client version required to be allowed to log in */
 #define MIN_VERSION_MAJOR	4
