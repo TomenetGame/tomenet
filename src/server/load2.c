f117648d5410bc2723ead653e40dcdfa220fd9ad
C. Blue <mail@c-blue.de>
2013-05-18 15:48:36 +0200

    oops

--- src/server/load2.c
+++ src/server/load2.c
@@ -3190,7 +3190,7 @@ void condense_max_depth(player_type *p_ptr) {
 	   (empty entries aka 0,0,0 should only occur tailing the other entries) */
 	for (i = 0; i < MAX_D_IDX * 2; i++) {
 		/* hack: fix IDDC to zero */
-		if (p_ptr->max_depth_wx[i] == WPOS_IRONDEEPDIVE_X & p_ptr->max_depth_wy[i] == WPOS_IRONDEEPDIVE_Y
+		if (p_ptr->max_depth_wx[i] == WPOS_IRONDEEPDIVE_X && p_ptr->max_depth_wy[i] == WPOS_IRONDEEPDIVE_Y
 		    && (p_ptr->max_depth_tower[i] ? 1 : -1) == WPOS_IRONDEEPDIVE_Z) {
 			p_ptr->max_depth[i] = 0;
 			continue;
