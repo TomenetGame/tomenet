13586edcdfc3cd68efa8390d3104f3595d5749d3
C. Blue <mail@c-blue.de>
2013-10-24 15:37:37 +0200

    spelling

--- src/server/store.c
+++ src/server/store.c
@@ -5632,13 +5632,13 @@ void reward_deed_item(int Ind, int item)
 	case SV_DEED_HIGHLANDER: /* winner's deed */
 		create_reward(Ind, o_ptr, 95, 95, TRUE, TRUE, RESF_LOW2, 3000); /* 95 is default depth for highlander tournament */
 		o_ptr->note = quark_add("Highlander reward");
-		msg_print(Ind, "\377GThe mayor's secretary hands you a reward, while everyone applaudes!");
+		msg_print(Ind, "\377GThe mayor's secretary hands you a reward, while everyone applauds!");
 		msg_print_near(Ind, "You hear some applause coming out of the mayor's office!");
 		break;
 	case SV_DEED_DUNGEONKEEPER: /* winner's deed */
 		create_reward(Ind, o_ptr, 95, 95, TRUE, TRUE, RESF_LOW2, 3000);
 		o_ptr->note = quark_add("Dungeon Keeper reward");
-		msg_print(Ind, "\377GThe mayor's secretary hands you a reward, while everyone applaudes!");
+		msg_print(Ind, "\377GThe mayor's secretary hands you a reward, while everyone applauds!");
 		msg_print_near(Ind, "You hear some applause coming out of the mayor's office!");
 		break;
 	case SV_DEED2_HIGHLANDER: /* participant's deed */
@@ -5649,21 +5649,21 @@ void reward_deed_item(int Ind, int item)
 	case SV_DEED_PVP_MAX:
 		create_reward(Ind, o_ptr, 95, 95, TRUE, TRUE, RESF_HIGH, 3000);
 		o_ptr->note = quark_add("PvP reward");
-		msg_print(Ind, "\377GThe mayor's secretary hands you a reward, while everyone applaudes!");
+		msg_print(Ind, "\377GThe mayor's secretary hands you a reward, while everyone applauds!");
 		msg_print_near(Ind, "You hear some applause coming out of the mayor's office!");
 		dis = 0;
 		break;
 	case SV_DEED_PVP_MID:
 		create_reward(Ind, o_ptr, 95, 95, TRUE, TRUE, RESF_MID, 3000);
 		o_ptr->note = quark_add("PvP reward");
-		msg_print(Ind, "\377GThe mayor's secretary hands you a reward, while everyone applaudes!");
+		msg_print(Ind, "\377GThe mayor's secretary hands you a reward, while everyone applauds!");
 		msg_print_near(Ind, "You hear some applause coming out of the mayor's office!");
 		dis = 0;
 		break;
 	case SV_DEED_PVP_MASS:
 		create_reward(Ind, o_ptr, 95, 95, TRUE, TRUE, RESF_MID, 3000);
 		o_ptr->note = quark_add("PvP reward");
-		msg_print(Ind, "\377GThe mayor's secretary hands you a reward, while everyone applaudes!");
+		msg_print(Ind, "\377GThe mayor's secretary hands you a reward, while everyone applauds!");
 		msg_print_near(Ind, "You hear some applause coming out of the mayor's office!");
 		dis = 0;
 		break;
