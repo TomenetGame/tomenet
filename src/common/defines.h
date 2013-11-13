ce0a2e447e9f8109d3ed2c2b88c65138a943b2d8
C. Blue <mail@c-blue.de>
2013-11-13 01:25:38 +0100

    bump client version to 4.5.5a

--- src/common/defines.h
+++ src/common/defines.h
@@ -41,7 +41,7 @@
 #define VERSION_MAJOR		4
 #define VERSION_MINOR		5
 #define VERSION_PATCH		5
-#define VERSION_EXTRA		0
+#define VERSION_EXTRA		1
 #define VERSION_BRANCH		0
 #define VERSION_BUILD		0
 
@@ -71,7 +71,7 @@
 
 /* Client-side only: Client release version tag
    (such as "a", "b" etc) used in window title and file dumps */
-#define CLIENT_VERSION_TAG	""
+#define CLIENT_VERSION_TAG	"a"
 
 /* Minimum client version required to be allowed to log in */
 #define MIN_VERSION_MAJOR	4
