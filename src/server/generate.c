bb310a1cec9b38f1977ebac8b7cf12d44fb33bbd
C. Blue <mail@c-blue.de>
2013-12-08 09:14:30 +0100

    max spawn chance for iddc deepsupply store

--- src/server/generate.c
+++ src/server/generate.c
@@ -10239,9 +10239,10 @@ for(mx = 1; mx < 131; mx++) {
 			dungeon_store2_timer = 2 + rand_int(cfg.dungeon_shop_timeout); /* reset timeout (in minutes) */
 	/* build only one special shop in the Nether Realm */
 	} else if (((dun_lev - 166) % 5 != 0) || (dun_lev == netherrealm_end)) return;
+
 	/* Try to create a dungeon store */
-	if ((rand_int(1000) < cfg.dungeon_shop_chance) ||
-	    netherrealm_level || (build_special_store == 3 || build_special_store == 4)) {
+	if ((rand_int(1000) < cfg.dungeon_shop_chance) || netherrealm_level ||
+	    (build_special_store == 3 || build_special_store == 4 || build_special_store == 5)) {
 		/* Try hard to place one */
 		for (i = 0; i < 300; i++) {
 			y = rand_int(dun->l_ptr->hgt-4)+2;
