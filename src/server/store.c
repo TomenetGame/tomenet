9d6cddf552f9c25b5534c9c23a300028ff759c4e
C. Blue <mail@c-blue.de>
2014-02-28 02:19:15 +0100

    entering a list house no longer attempts to auto-own all items as if the character tries to pick them all up

--- src/server/store.c
+++ src/server/store.c
@@ -5491,7 +5491,7 @@ static void display_house_entry(int Ind, int pos, house_type *h_ptr)
 	if (o_ptr->tval == TV_BOOK) attr = get_book_name_color(Ind, o_ptr);
 	
 	/* Let's fade out the items we CAN'T use too inside our own houses */
-	if (!can_use(Ind, o_ptr)) attr = TERM_L_DARK;
+	if (!can_use_admin(Ind, o_ptr)) attr = TERM_L_DARK;
 
 	/* Only show the weight of an individual item */
 	wgt = o_ptr->weight;
