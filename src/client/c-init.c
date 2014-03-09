d7f5e12afe59726880448317aa63715cf25d0a20
C. Blue <mail@c-blue.de>
2014-03-09 17:23:25 +0100

    reorder level before ridx in monster lore display

--- src/client/c-init.c
+++ src/client/c-init.c
@@ -537,12 +537,12 @@ void monster_lore_aux(int ridx, int rlidx, char paste_lines[18][MSG_LEN]) {
 
 		/* name */
 		//Term_putstr(5, 5, -1, TERM_YELLOW, p2 + 1);
-		strcpy(paste_lines[++pl], format("\377y%s (\377%c%c\377y, %d, L%d) ", /* need 1 space at the end to overwrite 'search result' */
+		strcpy(paste_lines[++pl], format("\377y%s (\377%c%c\377y, L%d, %d) ", /* need 1 space at the end to overwrite 'search result' */
 			monster_list_name[rlidx],
 			monster_list_symbol[rlidx][0],
 			monster_list_symbol[rlidx][1],
-			ridx,
-			monster_list_level[rlidx]));
+			monster_list_level[rlidx],
+			ridx));
 		Term_putstr(5, 5, -1, TERM_YELLOW, paste_lines[pl] + 2); /* no need for \377y */
 
 		/* fetch diz */
@@ -666,12 +666,12 @@ void monster_stats_aux(int ridx, int rlidx, char paste_lines[18][MSG_LEN]) {
 
 		/* name */
 		//Term_putstr(5, 5, -1, TERM_YELLOW, p2 + 1);
-		strcpy(paste_lines[++pl], format("\377y%s (\377%c%c\377y, %d, L%d) ", /* need 1 space at the end to overwrite 'search result' */
+		strcpy(paste_lines[++pl], format("\377y%s (\377%c%c\377y, L%d, %d) ", /* need 1 space at the end to overwrite 'search result' */
 			monster_list_name[rlidx],
 			monster_list_symbol[rlidx][0],
 			monster_list_symbol[rlidx][1],
-			ridx,
-			monster_list_level[rlidx]));
+			monster_list_level[rlidx],
+			ridx));
 		Term_putstr(5, 5, -1, TERM_YELLOW, paste_lines[pl] + 2); /* no need for \377y */
 
 		/* fetch stats: I/W/E/O/B/F/S lines */
