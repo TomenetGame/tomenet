a7fcfaea0dbc100e1c031cc76a6ab720faaea2b6
C. Blue <mail@c-blue.de>
2014-04-27 16:03:49 +0200

    make dice/card msg more distinguishable from spam of ppl joining/leaving the server

--- src/common/colours.h
+++ src/common/colours.h
@@ -58,7 +58,7 @@
 /* Misc event messages */
 #define COLOUR_DUNGEON		'u' /* enter/leave dungeon or tower */
 #define COLOUR_SERVER		'U' /* enter/leave the server */
-#define COLOUR_GAMBLE		'U' /* throwing dice, flipping coins, dealing cards */
+#define COLOUR_GAMBLE		's' /* throwing dice, flipping coins, dealing cards */
 
 /* Chat modes (server-side) */
 #define COLOUR_CHAT		'B'
