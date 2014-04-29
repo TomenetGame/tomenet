364862459ae9fb8460fae19acda3cb8d732bfe56
C. Blue <mail@c-blue.de>
2014-04-29 15:41:38 +0200

    mention activatable item in macro wizard

--- src/client/c-util.c
+++ src/client/c-util.c
@@ -3962,7 +3962,7 @@ Chain_Macro:
 					Term_putstr(10, 17, -1, TERM_L_GREEN, "i) Use a fighting technique (most melee classes)");
 					Term_putstr(10, 18, -1, TERM_L_GREEN, "j) Use a shooting technique (archers and rangers)");
 					Term_putstr(10, 19, -1, TERM_L_GREEN, "k) Set up a monster trap");
-					Term_putstr(10, 20, -1, TERM_L_GREEN, "l) Use a magic device");
+					Term_putstr(10, 20, -1, TERM_L_GREEN, "l) Use a magic device or activate an item");
 					Term_putstr(10, 21, -1, TERM_L_GREEN, "m)/M) Use a basic ability ('m') without/with target");
 					Term_putstr(10, 22, -1, TERM_L_GREEN, "n) Enter a custom action (same as pressing 'a' in macro screen)");
 					Term_putstr(10, 23, -1, TERM_L_GREEN, "o) Load a macro file");
