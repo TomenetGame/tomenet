edb2dce1f712a73cc367451e8e522213edfbc809
C. Blue <mail@c-blue.de>
2014-05-20 02:33:31 +0200

    do not message about outdated audio packs if there are actually none installed at all

--- src/server/nserver.c
+++ src/server/nserver.c
@@ -2513,8 +2513,8 @@ static int Handle_login(int ind)
 		msg_print(NumPlayers, "\374\377D --- Your music pack is outdated. ---");
 #else
 	/* In 4.5.7 we can now distinguish (client-side) between disabled and unavailable audio */
-	if (p_ptr->audio_sfx < __audio_sfx_max) msg_print(NumPlayers, "\374\377D --- Warning: Your sound pack is outdated! ---");
-	if (p_ptr->audio_mus < __audio_mus_max) msg_print(NumPlayers, "\374\377D --- Warning: Your music pack is outdated! ---");
+	if (p_ptr->audio_sfx > 3 && p_ptr->audio_sfx < __audio_sfx_max) msg_print(NumPlayers, "\374\377D --- Warning: Your sound pack is outdated! ---");
+	if (p_ptr->audio_mus && p_ptr->audio_mus < __audio_mus_max) msg_print(NumPlayers, "\374\377D --- Warning: Your music pack is outdated! ---");
 #endif
 
 	/* Admin messages */
