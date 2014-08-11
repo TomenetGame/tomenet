500df64bb4280e6dd003b82916d8062c840e6fc3
C. Blue <mail@c-blue.de>
2014-08-12 00:04:09 +0200

    make the audio master hotkey available on roguelike keymap, oops (CTRL+V)

--- src/client/c-util.c
+++ src/client/c-util.c
@@ -1156,7 +1156,8 @@ static char roguelike_commands(char command)
                 case KTRL('U'): hack_dir = 9; return ('+');
 
 		/* Oops, audio mixer */
-                case KTRL('F'): return (KTRL('U'));
+		case KTRL('F'): return (KTRL('U'));
+		case KTRL('V'): return (KTRL('N'));
 
                 /* Hack -- White-space */
                 case KTRL('M'): return ('\r');
@@ -6918,11 +6919,17 @@ void interact_audio(void) {
 			else if (audio_sfx <= 3 && audio_music > 0) Term_putstr(12, 4, -1, TERM_YELLOW, "Music pack detected. No sound pack seems to be installed.");
 			else Term_putstr(12, 4, -1, TERM_L_RED,                                         "Neither sound pack nor music pack seems to be installed. ");
 
-			Term_putstr(3, y_label + 2, -1, TERM_SLATE, "Outside of this mixer you can toggle audio and music by CTRL+N and CTRL+C.");
+			if (c_cfg.rogue_like_commands)
+				Term_putstr(3, y_label + 2, -1, TERM_SLATE, "Outside of this mixer you can toggle audio and music by CTRL+V and CTRL+C.");
+			else
+				Term_putstr(3, y_label + 2, -1, TERM_SLATE, "Outside of this mixer you can toggle audio and music by CTRL+N and CTRL+C.");
 
 			/* draw mixer */
 			Term_putstr(item_x[0], y_toggle, -1, TERM_WHITE, format(" [%s]", cfg_audio_master ? "\377GX\377w" : " "));
-			Term_putstr(item_x[0], y_toggle + 3, -1, TERM_SLATE, "CTRL+N");
+			if (c_cfg.rogue_like_commands)
+				Term_putstr(item_x[0], y_toggle + 3, -1, TERM_SLATE, "CTRL+V");
+			else
+				Term_putstr(item_x[0], y_toggle + 3, -1, TERM_SLATE, "CTRL+N");
 			Term_putstr(item_x[1], y_toggle, -1, TERM_WHITE, format(" [%s]", cfg_audio_music ? "\377GX\377w" : " "));
 			Term_putstr(item_x[1], y_toggle + 3, -1, TERM_SLATE, "CTRL+C");
 			Term_putstr(item_x[2], y_toggle, -1, TERM_WHITE, format(" [%s]", cfg_audio_sound ? "\377GX\377w" : " "));
@@ -7017,6 +7024,7 @@ void interact_audio(void) {
 			set_mixing();
 			break;
 		case KTRL('N'):
+		case KTRL('V'):
 		case 'a':
 			cfg_audio_master = !cfg_audio_master;
 			set_mixing();
