0dbdd67753f62accc5549551ab149b8e0a8b6277
C. Blue <mail@c-blue.de>
2015-05-03 09:51:25 +0200

    ------   Version bump to 4.6.0   ------

--- src/common/defines.h
+++ src/common/defines.h
@@ -39,19 +39,19 @@
 
 /* MAJOR/MINOR/PATCH version should be 0-15. */
 #define VERSION_MAJOR		4
-#define VERSION_MINOR		5
-#define VERSION_PATCH		9
+#define VERSION_MINOR		6
+#define VERSION_PATCH		0
 #define VERSION_EXTRA		0
 #define VERSION_BRANCH		0
-#define VERSION_BUILD		1
+#define VERSION_BUILD		0
 
 /* MAJOR/MINOR/PATCH version that counts as 'latest' (should be 0-15).
    If a player is online with a version > this && <= current version (VERSION_)
    it will be assumed that he's using a 'test client' and be marked in @ list
    with a 'T' marker which is visible only to admins.*/
 #define VERSION_MAJOR_LATEST	4
-#define VERSION_MINOR_LATEST	5
-#define VERSION_PATCH_LATEST	9
+#define VERSION_MINOR_LATEST	6
+#define VERSION_PATCH_LATEST	0
 #define VERSION_EXTRA_LATEST	0
 #define VERSION_BRANCH_LATEST	0
 #define VERSION_BUILD_LATEST	0
@@ -59,8 +59,8 @@
 /* maximum MAJOR/MINOR/PATCH version that counts as 'outdated' (should be 0-15). */
 #define VERSION_MAJOR_OUTDATED	4
 #define VERSION_MINOR_OUTDATED	5
-#define VERSION_PATCH_OUTDATED	8
-#define VERSION_EXTRA_OUTDATED	2
+#define VERSION_PATCH_OUTDATED	9
+#define VERSION_EXTRA_OUTDATED	0
 #define VERSION_BRANCH_OUTDATED	0
 #define VERSION_BUILD_OUTDATED	1 /* should always be 1 to invalidate previous 'test' versions */
 
