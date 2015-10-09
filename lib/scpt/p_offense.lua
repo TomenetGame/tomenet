288b665ad78c677fd42101970b5c881196ba1ae8
C. Blue <mail@c-blue.de>
2015-10-09 06:12:24 +0200

    buffed Exorcism damage, rather on par with Redemption now

--- lib/scpt/p_offense.lua
+++ lib/scpt/p_offense.lua
@@ -189,10 +189,10 @@ HEXORCISM_I = add_spell {
 	["fail"] = 	15,
 	["stat"] = 	A_WIS,
 	["spell"] = 	function(args)
-			dispel_demons(Ind, 50 + get_level(Ind, HEXORCISM_I, 350))
+			dispel_demons(Ind, 50 + get_level(Ind, HEXORCISM_I, 400))
 	end,
 	["info"] = 	function()
-		return "dam "..(50 + get_level(Ind, HEXORCISM_I, 350))
+		return "dam "..(50 + get_level(Ind, HEXORCISM_I, 400))
 	end,
 	["desc"] = 	{ "Dispels nearby demons.", }
 }
@@ -207,10 +207,10 @@ HEXORCISM_II = add_spell {
 	["fail"] = 	-90,
 	["stat"] = 	A_WIS,
 	["spell"] = 	function(args)
-			dispel_demons(Ind, 50 + get_level(Ind, HEXORCISM_I, 700))
+			dispel_demons(Ind, 50 + get_level(Ind, HEXORCISM_I, 800))
 	end,
 	["info"] = 	function()
-		return "dam "..(50 + get_level(Ind, HEXORCISM_I, 700))
+		return "dam "..(50 + get_level(Ind, HEXORCISM_I, 800))
 	end,
 	["desc"] = 	{ "Dispels nearby demons.", }
 }
