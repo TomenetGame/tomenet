dc38d60aef194fe0ad7a20379944c3d6d868d5b4
C. Blue <mail@c-blue.de>
2015-12-20 09:27:05 +0100

    --------  version bump to 4.6.1  --------

--- src/common/defines.h
+++ src/common/defines.h
@@ -40,8 +40,8 @@
 /* MAJOR/MINOR/PATCH version should be 0-15. BUILD == 1 means 'test build' */
 #define VERSION_MAJOR		4
 #define VERSION_MINOR		6
-#define VERSION_PATCH		0
-#define VERSION_EXTRA		1
+#define VERSION_PATCH		1
+#define VERSION_EXTRA		0
 #define VERSION_BRANCH		0
 #define VERSION_BUILD		0
 
@@ -51,16 +51,16 @@
    with a 'T' marker which is visible only to admins.*/
 #define VERSION_MAJOR_LATEST	4
 #define VERSION_MINOR_LATEST	6
-#define VERSION_PATCH_LATEST	0
+#define VERSION_PATCH_LATEST	1
 #define VERSION_EXTRA_LATEST	0
 #define VERSION_BRANCH_LATEST	0
 #define VERSION_BUILD_LATEST	0
 
 /* maximum MAJOR/MINOR/PATCH version that counts as 'outdated' (should be 0-15). */
 #define VERSION_MAJOR_OUTDATED	4
-#define VERSION_MINOR_OUTDATED	5
-#define VERSION_PATCH_OUTDATED	9
-#define VERSION_EXTRA_OUTDATED	0
+#define VERSION_MINOR_OUTDATED	6
+#define VERSION_PATCH_OUTDATED	0
+#define VERSION_EXTRA_OUTDATED	1
 #define VERSION_BRANCH_OUTDATED	0
 #define VERSION_BUILD_OUTDATED	1 /* should always be 1 to invalidate previous 'test' versions */
 
