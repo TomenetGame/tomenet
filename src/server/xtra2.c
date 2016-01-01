6abc26699dfec5ce5b4aaa52f674fd96b6840ac1
C. Blue <mail@c-blue.de>
2016-01-01 15:48:53 +0100

    aquatic monsters don't drop fireworks

--- src/server/xtra2.c
+++ src/server/xtra2.c
@@ -6153,6 +6153,7 @@ if (cfg.unikill_format) {
 	if (season_newyearseve &&
 	    ((r_ptr->flags3 & (RF3_ORC | RF3_TROLL | RF3_GIANT)) || /*note: demons/undead don't do fireworks..*/
 	    (strchr("hHkpty", r_ptr->d_char))) &&
+	    !(r_ptr->flags7 & RF7_AQUATIC) &&
 #if 0
 	    do_item &&
 	    ((r_ptr->flags1 & (RF1_DROP_60 | RF1_DROP_90 | RF1_DROP_1D2 | RF1_DROP_2D2 | RF1_DROP_3D2 | RF1_DROP_4D2))
