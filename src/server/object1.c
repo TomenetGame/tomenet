c1373387bd511d5f4ce440db9d567578111b5f3d
C. Blue <mail@c-blue.de>
2016-01-09 13:22:19 +0100

    HOME_APPRAISAL now also works for HF_TRAD houses

--- src/server/object1.c
+++ src/server/object1.c
@@ -4726,7 +4726,8 @@ bool identify_combo_aux(int Ind, object_type *o_ptr, bool full) {
 
 #ifdef PLAYER_STORES
  #ifdef HOME_APPRAISAL
-		if (inside_house(&p_ptr->wpos, p_ptr->px, p_ptr->py))
+		if (inside_house(&p_ptr->wpos, p_ptr->px, p_ptr->py) || p_ptr->store_num == STORE_HOME)
+		//if (istownarea(&p_ptr->wpos, MAX_TOWNAREA))
 			fprintf(fff, "This item would be appraised at %ld Au if put up for sale in your store.\n", (long int)price_item_player_store(Ind, o_ptr));
  #endif
 #endif
@@ -5359,7 +5360,8 @@ bool identify_combo_aux(int Ind, object_type *o_ptr, bool full) {
 
 #ifdef PLAYER_STORES
  #ifdef HOME_APPRAISAL
-	if (inside_house(&p_ptr->wpos, p_ptr->px, p_ptr->py))
+	if (inside_house(&p_ptr->wpos, p_ptr->px, p_ptr->py) || p_ptr->store_num == STORE_HOME)
+	//if (istownarea(&p_ptr->wpos, MAX_TOWNAREA))
 		fprintf(fff, "This item would be appraised at %ld Au if put up for sale in your store.\n", (long int)price_item_player_store(Ind, o_ptr));
  #endif
 #endif
