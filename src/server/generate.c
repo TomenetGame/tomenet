c34e6421ba45881595cd40f9188bba1313ed967a
C. Blue <mail@c-blue.de>
2016-06-22 17:27:36 +0200

    removed hardcoding

--- src/server/generate.c
+++ src/server/generate.c
@@ -9937,8 +9937,7 @@ static void cave_gen(struct worldpos *wpos, player_type *p_ptr) {
 			}
 
 			/* Hack -- add *more* downstairs for Highlander Tournament event */
-			if (sector00separation && wpos->wx == WPOS_HIGHLANDER_X && wpos->wy == WPOS_HIGHLANDER_Y &&
-			    wpos->wz * WPOS_HIGHLANDER_Z > 0 && dun_lev > COMFORT_PASSAGE_DEPTH) {
+			if (sector00separation && in_highlander(wpos) && dun_lev > COMFORT_PASSAGE_DEPTH) {
 				/* Place 3 or 4 down stairs near some walls */
 				alloc_stairs(wpos, (d_ptr->flags1 & DF1_FLAT) ? FEAT_WAY_MORE : FEAT_MORE, rand_range(2, 4), 2, NULL);
 			}
