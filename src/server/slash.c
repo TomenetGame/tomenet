103376080fe5d707c862e1c09f18c2707c7fe51a
C. Blue <mail@c-blue.de>
2016-08-06 16:34:40 +0200

    /wh vs /who disambiguity

--- src/server/slash.c
+++ src/server/slash.c
@@ -444,7 +444,7 @@ void do_slash_cmd(int Ind, char *message) {
 		return;
 // :)		break_cloaking(Ind, 3);
 	}
-	else if (prefix(message, "/whisper") || prefix(message, "/wh")) {
+	else if (prefix(message, "/whisper") || (prefix(message, "/wh") && !prefix(message, "/who"))) {
 		if (colon++) {
 			censor_message = TRUE;
 			censor_length = strlen(colon);
