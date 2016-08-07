0972f970e09a19e5ddf33ed5b44be5ed842f4a2c
C. Blue <mail@c-blue.de>
2016-08-07 04:14:52 +0200

    my local TEST_SERVER won't compile w/o this; works for pm though for some reason

--- src/common/z-util.c
+++ src/common/z-util.c
@@ -195,7 +195,8 @@ void (*quit_aux)(cptr) = NULL;
  * Otherwise, plog() 'str' and exit with an error code of -1.
  * But always use 'quit_aux', if set, before anything else.
  */
-#ifdef TEST_CLIENT /* all RETRY_LOGIN stuff here is for client-side only */
+//#ifdef TEST_CLIENT /* all RETRY_LOGIN stuff here is for client-side only */
+#if defined(TEST_CLIENT) || defined(TEST_SERVER) /* need to define for TEST_SERVER too, or it won't compile */
  #define RETRY_LOGIN
 #endif
 #ifdef RETRY_LOGIN
