0b3b433d65bf12b5ca77250b2078aaa2142aa311
C. Blue <mail@c-blue.de>
2016-09-06 07:42:46 +0200

    added defines-features-local.h to #undef features locally (for testing purpose)

---
+++ src/common/defines-features-local.h
@@ -0,0 +1,5 @@
+/* Purpose: Define and especially undefine features.
+   Features are defined after defines-local.h has been processed, so they
+   cannot be undefined in defines-local.h. Use this file instead.
+   Included exclusively by "defines-features.h". */
+
