398c84ef23d82f1a23075f19517d359fc5937490
C. Blue <mail@c-blue.de>
2017-02-04 07:31:28 +0100

    mh

--- lib/scpt/dr_physical.lua
+++ lib/scpt/dr_physical.lua
@@ -208,10 +208,10 @@ FOCUSSHOT = add_spell {
 	["stat"] = 	A_WIS,
 	["direction"] = FALSE,
 	["spell"] = 	function()
-			do_focus_shot(Ind, get_level(Ind, FOCUSSHOT, 25), rand_int(5) + 15 + get_level(Ind, FOCUSSHOT, 10))
+			do_focus_shot(Ind, 5 + get_level(Ind, FOCUSSHOT, 20), rand_int(5) + 15 + get_level(Ind, FOCUSSHOT, 10))
 			end,
 	["info"] = 	function()
-			return "+" .. get_level(Ind, FOCUSSHOT, 25) .. " dur d5+" .. (15 + get_level(Ind, FOCUSSHOT, 10))
+			return "+" .. (5 + get_level(Ind, FOCUSSHOT, 20)) .. " dur d5+" .. (15 + get_level(Ind, FOCUSSHOT, 10))
 			end,
 	["desc"] = 	{ "Increases your accuracy.", }
 }
