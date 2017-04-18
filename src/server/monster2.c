ee420daf5c4baaa8c438b7225553d06b1016a7ae
C. Blue <mail@c-blue.de>
2017-04-18 09:15:42 +0200

    (astar log msg)

--- src/server/monster2.c
+++ src/server/monster2.c
@@ -630,7 +630,7 @@ void compact_monsters(int size, bool purge) {
 			if (m_list[i].r_idx /* alive monster? */
 			    && m_list[i].astar_idx != -1) {
 				astar_info_open[m_list[i].astar_idx].m_idx = i;
-				s_printf("ASTAR_COMPACT_MONSTERS: Reassigned m_idx %d to %d.\n", i, m_list[i].astar_idx);
+				s_printf("ASTAR_COMPACT_MONSTERS: Reassigned m_idx %d to slot %d.\n", i, m_list[i].astar_idx);
 			}
 #endif
 
