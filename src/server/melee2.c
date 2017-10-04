912f1948961489b08cc45462f6b09bfba475d51b
C. Blue <mail@c-blue.de>
2017-10-04 11:45:58 +0200

    (better)

--- src/server/melee2.c
+++ src/server/melee2.c
@@ -2097,9 +2097,9 @@ bool make_attack_spell(int Ind, int m_idx) {
 
 	/* Specialty for AI_HYBRID (Tzeentch) so he actually does melee for a change despite casting 1_IN_1 */
 	if ((r_ptr->flags3 & RF3_AI_HYBRID) && !(m_ptr->mind & HYBRID_ANNOY)
-	    && r_ptr->freq_innate && r_ptr->freq_spell) //avoid div0
+	    && chance) //avoid div0
 		/* Shift chance from 1_IN_n to 1_IN_(n+i/10), where i should probably be between 10 and 20. */
-		chance = 1000 / ((1000 / ((r_ptr->freq_innate + r_ptr->freq_spell) / 2)) + 10);
+		chance = 1000 / ((1000 / chance) + 10);
 
 	/* Only do spells occasionally */
 	if (rand_int(100) >= chance) return (FALSE);
