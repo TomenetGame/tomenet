48847b55f5d6515d2af885fc2446ffeaf11f9b56
C. Blue <mail@c-blue.de>
2018-01-04 07:32:56 +0100

    Quest framework special functions, called from q_info.txt (super-experimental).

---
+++ lib/scpt/quests.lua
@@ -0,0 +1,9 @@
+-- Purpose of this file is to provide special lua functions that are called by the
+-- quests framework in q_info.txt. As convention to keep things clean, these should
+-- all be named 'quest_' and end on the 'internal quest codename'.  - C. Blue
+
+-- Purpose: The Town Elder gives you advice based on your actual current character's details!
+function quest_towneltalk(msg)
+	lua_s_print("q-towneltalk: "..msg.."\n")
+	msg_print(Ind, "q-towneltalk: "..msg)
+end
