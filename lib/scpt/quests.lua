dba280f166159c863cb089fbef872c23acac6a8e
C. Blue <mail@c-blue.de>
2018-01-04 08:41:40 +0100

    Quest 'towneltalk' updated.

--- lib/scpt/quests.lua
+++ lib/scpt/quests.lua
@@ -4,15 +4,19 @@
 
 -- Purpose: The Town Elder gives you advice based on your actual current character's details!
 function quest_towneltalk(Ind, msg)
-	if player.pclass == CLASS_WARRIOR or player.pclass == CLASS_PALADIN or player.pclass == CLASS_MIMIC or player.pclass == CLASS_ROGUE or player.admin_dm ~= 0 then
+	if player.admin_dm ~= 0 then admin = 1 else admin = 0 end
+	if player.pclass == CLASS_WARRIOR or player.pclass == CLASS_PALADIN or player.pclass == CLASS_MIMIC or player.pclass == CLASS_ROGUE or admin then
 		if player.num_blow < 2 then
-			msg_print(Ind, "\255UYour number of blows per round (BpR) is too low, it should at least be 2!")
+			msg_print(Ind, "\255UFor a "..msg.." you attack too slowly. Your number of blows per round (BpR) should at least be 2!")
 			msg_print(Ind, "\255UYou should try getting the lightest weapon possible, to remedy this! For example a dagger, whip, or maybe a spear or cleaver if you specialized in those.")
 			msg_print(Ind, "\255UIf this doesn't give you at least 2 BpR, you should really increase your strength, or maybe even your dexterity, as these might just be too low.")
+			if player.pclass == CLASS_ROGUE or admin then
+				msg_print(Ind, "\255UAs a rogue a third way is to dual-wield, by equipping two weapons at once!")
+			end
 		else
-			msg_print(Ind, "\255UYou seem to be doing fine.")
+			msg_print(Ind, "\255UYou seem to be doing fine, "..msg..".")
 		end
 	else
-		msg_print(Ind, "\255UYou seem to be doing fine.")
+		msg_print(Ind, "\255UYou seem to be doing fine, "..msg..".")
 	end
 end
