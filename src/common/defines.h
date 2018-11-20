a0e871171c8fb434884cdbf778dad40ee42e413e
C. Blue <mail@c-blue.de>
2018-11-20 17:44:30 +0100

    --- Bumped version to 4.7.2 ---

--- src/common/defines.h
+++ src/common/defines.h
@@ -40,8 +40,8 @@
 /* MAJOR/MINOR/PATCH version should be 0-15. BUILD == 1 means 'test build' */
 #define VERSION_MAJOR		4
 #define VERSION_MINOR		7
-#define VERSION_PATCH		1
-#define VERSION_EXTRA		2
+#define VERSION_PATCH		2
+#define VERSION_EXTRA		0
 #define VERSION_BRANCH		0
 #define VERSION_BUILD		0
 
@@ -51,8 +51,8 @@
    with a 'T' marker which is visible only to admins.*/
 #define VERSION_MAJOR_LATEST	4
 #define VERSION_MINOR_LATEST	7
-#define VERSION_PATCH_LATEST	1
-#define VERSION_EXTRA_LATEST	1
+#define VERSION_PATCH_LATEST	2
+#define VERSION_EXTRA_LATEST	0
 #define VERSION_BRANCH_LATEST	0
 #define VERSION_BUILD_LATEST	0
 
@@ -60,7 +60,7 @@
 #define VERSION_MAJOR_OUTDATED	4
 #define VERSION_MINOR_OUTDATED	7
 #define VERSION_PATCH_OUTDATED	1
-#define VERSION_EXTRA_OUTDATED	0
+#define VERSION_EXTRA_OUTDATED	1
 #define VERSION_BRANCH_OUTDATED	0
 #define VERSION_BUILD_OUTDATED	1 /* should always be 1 to invalidate previous 'test' versions */
 
@@ -70,7 +70,7 @@
 
 /* Client-side only: Client release version tag
    (such as "a", "b" etc) used in window title and file dumps */
-#define CLIENT_VERSION_TAG	"b"
+#define CLIENT_VERSION_TAG	""
 
 
 /* Minimum client version required to be allowed to log in */
