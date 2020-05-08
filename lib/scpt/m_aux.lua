ccac067bc92a8b4c85f7c781542c4294d86fd446
C. Blue <mail@c-blue.de>
2020-05-08 16:11:08 +0200

    Possible luarization of mimic powers in the future.

---
+++ lib/scpt/m_aux.lua
@@ -0,0 +1,402 @@
+-- Server side functions to help with the finally luarized mimic powers - C. Blue
+
+__tmp_powers = {}
+__tmp_powers_num = 0
+
+
+-- define get_check2() for older clients
+if rawget(globals(), "power_get_check2") == nil then
+	function power_get_check2(prompt, defaultyes)
+		return power_get_check(prompt)
+	end
+end
+
+
+-- TomeNET additional
+-- Get the amount of mana(or power) needed
+function power_need_direction(s)
+	return power(s).direction
+end
+
+function power_need_item(s)
+	return power(s).item
+end
+
+function add_power(s)
+	__tmp_powers[__tmp_powers_num] = s
+
+	__tmp_powers_num = __tmp_powers_num + 1
+	return (__tmp_powers_num - 1)
+end
+
+function finish_power(must_i)
+	local i, s
+
+	s = __tmp_powers[must_i]
+
+	assert(s.name, "No power name!")
+	assert(s.name2, "No short name!")
+	if not s.level then s.level = 0 end
+	assert(s.mana, "No power mana!")
+	if not s.mana_max then s.mana_max = s.mana end
+	assert(s.fail, "No power failure rate!")
+	assert(s.spell, "No power function!")
+	assert(s.info, "No power info!")
+	assert(s.desc, "No power desc!")
+	if not s.direction then s.direction = FALSE end
+	if not s.item then s.item = -1 end
+
+	i = new_power(must_i, s.name)
+	assert(i == must_i, "ACK ! i != must_i ! please contact the maintainer")
+	power(i).mana = s.mana
+	power(i).mana_max = s.mana_max
+	power(i).fail = s.fail
+	power(i).skill_level = s.level
+	if type(s.spell_power) == "number" then
+		power(i).spell_power = s.spell_power
+	else
+		power(i).spell_power = 0
+	end
+	__power_spell[i] = s.spell
+	__power_name2[i] = s.name2
+	__power_info[i] = s.info
+	__power_desc[i] = s.desc
+	return i
+end
+
+-- Creates the power arrays
+__power_spell = {}
+__power_name2 = {}
+__power_info = {}
+__power_desc = {}
+
+-- Find a spell by name
+function find_power(name)
+	local i
+
+	i = 0
+	while (i < __tmp_powers_num) do
+		if __tmp_powers[i].name == name then return i end
+		i = i + 1
+	end
+	return -1
+end
+
+-- Can we use the power? (Note: Usually all powers have level 0 so we can always use them)
+function power_get_level(i, s)
+	return players(i).s_info[SKILL_MIMICRY + 1].value / 1000 - spells(s).level + 1
+end
+function is_ok_power(i, s)
+	if power_get_level(i, s) >= 1 then return 1 end
+	return nil
+end
+function is_available_power(i, s)
+	--if (!(players(i).innate_spells[power[s].flag_block] & (1U << power[s].flag_index))) return FALSE;
+	if power_get_level(i, s) >= 1 then return 1 end
+	return nil
+end
+
+-- Get the amount of mana(or power) needed
+function power_get_mana(i, s)
+	return power(s).mana
+end
+
+-- Return the amount of power(mana, piety, whatever) for the spell
+function power_get_power(i, s)
+	-- client-side (0) or server-side (>=1) ?
+	if i ~= 0 then
+		player = players(i)
+	end
+	return player.csp
+end
+
+-- Changes the amount of power(mana, piety, whatever) for the spell
+function power_adjust_power(i, s, x)
+	power_increase_mana(i, x)
+end
+
+-- Print the power list
+-- XXX client only
+function print_powers2(i)
+	local x, y, index, size, s
+
+	x = 0
+	y = 2
+	size = 0
+
+	-- Parse all powers
+	for index, s in __tmp_powers do
+		local color = TERM_L_DARK
+		local lvl = power_get_level(i, s)
+
+		if is_ok_power(i, s) then
+			if power_get_mana(i, s) > power_get_power(i, s) then color = TERM_ORANGE
+			else color = TERM_L_GREEN end
+		end
+
+		c_prt(color, format("%c) %-22s %4s %3d%s %s", size + strbyte("a"), power(s).name, power_get_mana(i, s), power_chance(i, s), "%", __power_info[s]()), y, x)
+		y = y + 1
+		size = size + 1
+	end
+
+	prt(format("   %-22s Cost Fail Info", "Name"), 1, x)
+	return y
+end
+
+function print_custom_tome(i, inven_slot)
+	local x, y, index, sch, size, s, custom_book
+
+	x = 0
+	y = 2
+	size = 0
+
+	-- Parse all powers
+	for index, s in custom_book do
+		local color = TERM_L_DARK
+		local lvl = power_get_level(i, s, 50, -50)
+
+		if is_ok_power(i, s) then
+			if power_get_mana(i, s) > power_get_power(i, s) then color = TERM_ORANGE
+			else color = TERM_L_GREEN end
+		end
+
+		c_prt(color, format("%c) %-22s %4s %3d%s %s", size + strbyte("a"), power(s).name, power_get_mana(i, s), power_chance(i, s), "%", __power_info[s]()), y, x)
+		y = y + 1
+		size = size + 1
+	end
+
+	prt(format("   %-22s%-14s Level Cost Fail Info", "Name", "School"), 1, x)
+	return y
+end
+
+-- XXX client only
+function print_power_desc(s, y)
+	local index, desc, x
+
+	x = 0
+
+	if type(__power_desc[s]) == "string" then c_prt(TERM_L_BLUE, __power_desc[s], y, x)
+	else
+		for index, desc in __power_desc[s] do
+			c_prt(TERM_L_BLUE, desc, y, x)
+			y = y + 1
+		end
+	end
+	if power_check_affect(s, "piety", FALSE) then
+		c_prt(TERM_L_WHITE, "It uses piety to cast.", y, x)
+		y = y + 1
+	end
+	if not power_check_affect(s, "blind") then
+		c_prt(TERM_ORANGE, "It is castable even while blinded.", y, x)
+		y = y + 1
+	end
+	if not power_check_affect(s, "confusion") then
+		c_prt(TERM_ORANGE, "It is castable even while confused.", y, x)
+		y = y + 1
+	end
+
+	return y
+end
+
+-- Returns spell chance of failure for power
+function power_chance(i, s)
+	local chance, s_ptr
+	local player, ls
+
+	s_ptr = power(s)
+
+	--hack: LIMIT_SPELLS uses top-level failrate here instead of worse one we had at lower level!
+	-- client-side (0) or server-side (>=1) ?
+	if i ~= 0 then
+		player = players(i)
+		ls = player.limit_spells
+		player.limit_spells = 0
+	--client version recent enough to even know 'hack_force_spell_level'? (otherwise we'd get a lua error)
+	elseif (def_hack("hack_force_spell_level", nil)) then
+		ls = hack_force_spell_level
+		hack_force_spell_level = 0
+	end
+
+	-- Hack: "101" means 100% chance to succeed ('fail' is unsigned byte, so it'll be 157) - C. Blue
+	if (s_ptr.fail == 101) then
+		chance = 0
+	-- A new hack: "102" means greatly reduced fail chance (from 0 base fail chance) - C. Blue
+	elseif (s_ptr.fail == 102) then
+		chance = (lua_power_chance(i, 0, power_get_level(i, s, 50), s_ptr.skill_level, power_get_mana(i, s), power_get_power(i, s), get_power_stat(s)) + 5) / 6
+	else
+		-- Extract the base spell failure rate
+		chance = lua_power_chance(i, s_ptr.fail, power_get_level(i, s, 50), s_ptr.skill_level, power_get_mana(i, s), power_get_power(i, s), get_power_stat(s))
+	end
+
+	--unhack: LIMIT_SPELLS
+	-- client-side (0) or server-side (>=1) ?
+	if i ~= 0 then
+		player = players(i)
+		player.limit_spells = ls
+	--client version recent enough to even know 'hack_force_spell_level'? (otherwise we'd get a lua error)
+	elseif (def_hack("hack_force_spell_level", nil)) then
+		hack_force_spell_level = ls
+	end
+
+	-- Return the chance
+	return chance
+end
+
+function power_check_affect(s, name, default)
+	local s_ptr = __tmp_powers[s]
+	local a
+
+	if type(s_ptr[name]) == "number" then
+		a = s_ptr[name]
+	else
+		a = default
+	end
+
+	if a == FALSE then
+		return nil
+	else
+		return TRUE
+	end
+end
+
+-- Returns the stat to use for the power, INT by default
+function get_power_stat(s)
+	if not __tmp_powers[s].stat then return A_INT
+	else return __tmp_powers[s].stat end
+end
+
+-- XXX server only
+-- one question.. why this should be LUA anyway?
+-- because accessing lua table is so badly easier in lua
+function use_power(i, s, s_ptr, no_cost, other)
+	-- client-side (0) or server-side (>=1) ?
+	if i ~= 0 then
+		player = players(i)
+	end
+
+	local use = FALSE
+
+
+	-- No magic
+	if check_antimagic(Ind, get_power_am(s)) == TRUE then
+--Next line is already in the server sources.
+--		msg_print(i, "Your anti-magic field disrupts any magic attempts.")
+		local energy = level_speed(player.wpos);
+		player.energy = player.energy - energy
+		return 1 --continue ftk
+	end
+
+	-- TODO: check the ownership
+	-- uh?
+
+	-- if it costs something then some condition must be met
+	if not no_cost then
+		-- Require lite
+		if (power_check_affect(s, "blind")) and ((player.blind > 0) or (no_lite(Ind) == TRUE)) then
+			local energy = level_speed(player.wpos);
+			player.energy = player.energy - energy
+			msg_print(i, "You cannot see!")
+			return 0
+		end
+
+		-- Not when confused
+		if (power_check_affect(s, "confusion")) and (player.confused > 0) then
+			local energy = level_speed(player.wpos);
+			player.energy = player.energy - energy
+			msg_print(i, "You are too confused!")
+			return 0
+		end
+
+		-- Level requirements met?
+		if (power_get_level(i, s) < 1) then
+			msg_print(i, "Your skill is not high enough!")
+			lua_intrusion(i, "bad power level")
+			return 0
+		end
+
+		-- Enough mana
+		if (power_get_mana(i, s) > power_get_power(i, s)) then
+			local energy = level_speed(player.wpos);
+			player.energy = player.energy - energy
+--			if (get_check2("You do not have enough "..get_power_name(s)..", do you want to try anyway?", FALSE) == FALSE) then return end
+			msg_print(i, "You do not have enough mana to use "..power(s).name..".")
+				return 0
+		end
+
+--[[		-- Sanity check for direction
+		if (need_direction(s) && other.direction == -1) then
+			msg_print(i, "Spell needs a direction.")
+			return
+		end
+]]
+
+		-- Invoke the spell effect
+		if (magik(power_chance(i, s)) == FALSE) then
+			local mp_cost = power_get_mana(i, s)
+
+			--unlike for spells, stick with not giving an extra confirmation message for now..
+			--msg_print(i, "You successfully use "..spell(s).name..".")
+
+			-- Reduce mana BEFORE casting the spell, for Necromancy to work effectively:
+			-- If the monster dies, MP should not get refunded before the spell cost was actually deducted.
+			adjust_power(i, s, -mp_cost)
+			use  = TRUE
+
+			if (__power_spell[s](other) ~= nil) then
+				--correct the situation - we have to do it this way round,
+				--so we were able to deduct MP before actually casting the spell above
+				use = FALSE
+				--and refund the mana
+				adjust_power(i, s, mp_cost)
+			end
+		else
+			msg_print(i, "\255yYou failed to get the power "..power(s).name.." off!")
+			-- Reduce mana
+			adjust_power(i, s, -get_mana(i, s))
+			use  = TRUE
+		end
+	else
+		__power_spell[s](other)
+	end
+
+	if use == TRUE then
+		-- Take a turn
+		local energy = level_speed(player.wpos);
+		player.energy = player.energy - energy
+	end
+
+	player.redraw = bor(player.redraw, PR_MANA)
+	--player.window = bor(player.window, PW_PLAYER)
+	--player.window = bor(player.window, PW_SPELL)
+
+	return 1
+end
+
+function pre_exec_power_dir(s)
+	if __tmp_powers[s].direction then
+		if type(__tmp_powers[s].direction) == "function" then
+			return __tmp_powers[s].direction()
+		else
+			return __tmp_powers[s].direction
+		end
+	end
+end
+
+-- Returns the percentage of AM (Antimagic field) efficiency vs this power
+function get_power_am(s)
+	if not __tmp_powers[s].am then return 100
+	else return __tmp_powers[s].am end
+end
+
+-- Returns if a power is an attack power and therefore to be repeated in FTK mode - C. Blue
+-- note: 0 means no FTK, 1 means 'do FTK', 2 means 'do FTK && no need for monster-free LOS'
+function get_power_ftk(s)
+	if not __tmp_powers[s].ftk then return 0
+	else return __tmp_powers[s].ftk end
+end
+
+-- Returns if a power is used on players instead of monsters (friendly power)
+function get_power_friendly(s)
+	if not __tmp_powers[s].friendly then return 0
+	else return __tmp_powers[s].friendly end
+end
---
+++ lib/scpt/mimicry.lua
@@ -0,0 +1,385 @@
+-- Povide and handle mimic spells (monster form spells) - C. Blue
+
+--Note: So far mimic powers used INT for determining fail rate.
+--	["stat"] = 	A_INT,
+--Note: So far mimic powers always had a level of 0 and gave 0 exp on first usage.
+--	["level"] = 	0,
+--Note: So far all mimic powers were set to 100% AM-affected, compare check_antimagic() in do_cmd_mimic()
+--	["am"] = 	100,
+
+--misc notes..
+--fire_grid_bolt(Ind, GF_, args.dir, 5 + get_level(Ind, MIMICXXX, 100), " ")
+--return "power "..(5 + get_level(Ind, MIMICXXX, 100))
+
+
+MIMIC_SHRIEK = add_power {
+	["name"] = 	"Shriek",
+	["mana"] = 	2,
+	["fail"] = 	0,
+	["stat"] = 	A_INT,
+	["direction"] = FALSE,
+	["ftk"] =	0,
+	["spell"] = 	function(args)
+				shriek(Ind);
+			end,
+	["info"] = 	function()
+				return ""
+			end,
+	["desc"] = 	{ "You emit a shrill noise.", }
+}
+
+
+--[[
+Shriek                  Aggravates nearby creatures and annoys nearby players.
+Fire Rocket             ..
+Arrow                   Fires an arrow, does not require bow or ammunition.
+Shot                    Fires a shot, does not require bow or ammunition.
+Bolt                    Fires a bolt, does not require bow or ammunition.
+Missile                 Fires a (physical) missile.
+Breathe Acid            ..
+Breathe Lightning       ..
+Breathe Fire            ..
+Breathe Cold            ..
+Breathe Poison          ..
+Breathe Nether          ..
+Breathe Lite            ..
+Breathe Darkness        ..
+Breathe Confusion       ..
+Breathe Sound           ..
+Breathe Chaos           ..
+Breathe Disenchantment  ..
+Breathe Nexus           ..
+Breathe Time            ..
+Breathe Inertia         ..
+Breathe Gravity         ..
+Breathe Shards          ..
+Breathe Plasma          ..
+Breathe Force           ..
+Breathe Mana            ..
+Breathe Disintegration  ..
+Breathe Toxic Waste     ..
+Throw Boulder           Hurls a boulder at a target (giants do this usually).
+Acid Ball               ..
+Lightning Ball          ..
+Fire Ball               ..
+Cold Ball               ..
+Poison Ball             ..
+Nether Ball             ..
+Water Ball              ..
+Mana Storm              .. (casts a mana ball)
+Darkness Storm          .. (casts a darkness ball)
+Mind Blast              Psi attack, similar to Psionic Blast mindcrafter spell.
+Brain Smash             Powerful psi attack, similar to Psionic Blast.
+Cause Wounds            The curse spell, eg used by priests and archpriests.
+Toxic Waste Ball        ..
+Raw Chaos               .. (casts a chaos ball)
+Acid Bolt               ..
+Lightning Bolt          ..
+Fire Bolt               ..
+Cold Bolt               ..
+Poison Bolt             ..
+Nether Bolt             ..
+Water Bolt              ..
+Mana Bolt               ..
+Plasma Bolt             ..
+Ice Bolt                ..
+Magic Missile           ..
+Scare                   ..
+Blind                   ..
+Confusion               ..
+Slow                    ..
+Paralyze                Tries to put a monster into stasis (similar to sleep).
+Haste Self              Temporarily increases your speed by 10.
+Heal                    Heals yourself, amount depends on spell level.
+Blink                   Short range teleport, like Phase Door.
+Teleport                ..
+Teleport To             Tries to teleport a monster towards you.
+Teleport Away           Tries to teleport monsters away (it's a beam!).
+Darkness                Unlights the area around you.
+Cause Amnesia           Confuses a target.
+Disenchantment Bolt     ..
+Breathe Water           ..
+
+
+ {"Shriek", FALSE},
+  {"Negate magic", FALSE},
+  {"XXX", TRUE},
+  {"Fire Rocket", TRUE},
+
+  {"Arrow", TRUE},
+  {"Shot", TRUE},
+  {"Bolt", TRUE},
+  {"Missile", TRUE},
+
+  {"Breathe Acid", TRUE},
+  {"Breathe Lightning", TRUE},
+  {"Breathe Fire", TRUE},
+  {"Breathe Cold", TRUE},
+
+  {"Breathe Poison", TRUE},
+  {"Breathe Nether", TRUE},
+  {"Breathe Lite", TRUE},
+  {"Breathe Darkness", TRUE},
+
+  {"Breathe Confusion", TRUE},
+  {"Breathe Sound", TRUE},
+  {"Breathe Chaos", TRUE},
+  {"Breathe Disenchantment", TRUE},
+
+  {"Breathe Nexus", TRUE},
+  {"Breathe Time", TRUE},
+  {"Breathe Inertia", TRUE},
+  {"Breathe Gravity", TRUE},
+
+  {"Breathe Shards", TRUE},
+  {"Breathe Plasma", TRUE},
+  {"Breathe Force", TRUE},
+  {"Breathe Mana", TRUE},
+
+  {"Breathe Disintegration", TRUE},
+  {"Breathe Toxic Waste", TRUE},
+  {"Ghastly Moan", FALSE},
+  {"Throw Boulder", TRUE},      /* "XXX", */
+
+
+  {"Acid Ball", TRUE},
+  {"Lightning Ball", TRUE},
+  {"Fire Ball", TRUE},
+  {"Cold Ball", TRUE},
+
+  {"Poison Ball", TRUE},
+  {"Nether Ball", TRUE},
+  {"Water Ball", TRUE},
+  {"Mana Storm", TRUE},
+
+  {"Darkness Storm", TRUE},
+  {"Drain Mana", TRUE},
+  {"Mind Blast", TRUE},
+  {"Brain Smash", TRUE},
+
+  {"Cause Wounds", TRUE},
+  {"XXX", TRUE},
+  {"Toxic Waste Ball", TRUE},
+  {"Raw Chaos", TRUE},
+
+  {"Acid Bolt", TRUE},
+  {"Lightning Bolt", TRUE},
+  {"Fire Bolt", TRUE},
+  {"Cold Bolt", TRUE},
+
+  {"Poison Bolt", TRUE},
+  {"Nether Bolt", TRUE},
+  {"Water Bolt", TRUE},
+  {"Mana Bolt", TRUE},
+
+  {"Plasma Bolt", TRUE},
+  {"Ice Bolt", TRUE},
+  {"Magic Missile", TRUE},
+  {"Scare", TRUE},
+
+  {"Blind", TRUE},
+  {"Confusion", TRUE},
+  {"Slow", TRUE},
+  {"Paralyze", TRUE},
+
+
+  {"Haste Self", FALSE},
+  {"Hand of Doom", TRUE},
+  {"Heal", FALSE},
+  {"XXX", TRUE},
+
+  {"Blink", FALSE},
+  {"Teleport", FALSE},
+  {"XXX", TRUE},
+  {"XXX", TRUE},
+
+  {"Teleport To", TRUE},
+  {"Teleport Away", TRUE},
+  {"Teleport Level", FALSE},
+  {"XXX", TRUE},
+
+  {"Darkness", FALSE},
+  {"Trap Creation", FALSE},
+  {"Cause Amnesia", TRUE},
+
+
+  {"Disenchantment Bolt", TRUE},
+  {"Disenchantment Ball", TRUE},
+  {"Breathe Ice", TRUE},
+  {"Breathe Water", TRUE},
+
+
+* (slevel, smana, sfail, sexp, ftk)
+ */
+magic_type innate_powers[128] = {
+/* 0(lev), mana, fail, 0(exp), ftk */
+// RF4_SHRIEK                   0x00000001      /* Shriek for help */
+  {0, 2, 0, 0, 0},
+// RF4_UNMAGIC                  0x00000002      /* (?) */
+  {0, 0, 0, 0, 0},
+// (S_ANIMAL) RF4_XXX3          0x00000004      /* (?) */
+  {0, 0, 0, 0, 0},
+// RF4_ROCKET                   0x00000008      /* (?) */
+  {0, 60, 70, 0, 2},
+// RF4_ARROW_1                  0x00000010      /* Fire an arrow (light) */
+  {0, 2, 5, 0, 1},
+// RF4_ARROW_2                  0x00000020      /* Fire an shot (heavy) */
+  {0, 2, 6, 0, 1},
+// XXX (RF4_ARROW_3)            0x00000040      /* Fire bolt (heavy) */
+  {0, 2, 7, 0, 1},
+// XXX (RF4_ARROW_4)            0x00000080      /* Fire missiles (heavy) */
+  {0, 3, 9, 0, 1},
+// RF4_BR_ACID                  0x00000100      /* Breathe Acid */
+  {0, 10, 20, 0, 2},
+// RF4_BR_ELEC                  0x00000200      /* Breathe Elec */
+  {0, 10, 20, 0, 2},
+// RF4_BR_FIRE                  0x00000400      /* Breathe Fire */
+  {0, 10, 20, 0, 2},
+// RF4_BR_COLD                  0x00000800      /* Breathe Cold */
+  {0, 10, 20, 0, 2},
+// RF4_BR_POIS                  0x00001000      /* Breathe Poison */
+  {0, 13, 25, 0, 2},
+// RF4_BR_NETH                  0x00002000      /* Breathe Nether */
+  {0, 22, 35, 0, 2},
+// RF4_BR_LITE                  0x00004000      /* Breathe Lite */
+  {0, 10, 20, 0, 2},
+// RF4_BR_DARK                  0x00008000      /* Breathe Dark */
+  {0, 10, 20, 0, 2},
+// RF4_BR_CONF                  0x00010000      /* Breathe Confusion */
+  {0, 10, 25, 0, 2},
+// RF4_BR_SOUN                  0x00020000      /* Breathe Sound */
+  {0, 14, 30, 0, 2},
+// RF4_BR_CHAO                  0x00040000      /* Breathe Chaos */
+  {0, 25, 40, 0, 2},
+// RF4_BR_DISE                  0x00080000      /* Breathe Disenchant */
+  {0, 28, 45, 0, 2},
+// RF4_BR_NEXU                  0x00100000      /* Breathe Nexus */
+  {0, 20, 45, 0, 2},
+// RF4_BR_TIME                  0x00200000      /* Breathe Time */
+  {0, 30, 50, 0, 2},
+// RF4_BR_INER                  0x00400000      /* Breathe Inertia */
+  {0, 25, 35, 0, 2},
+// RF4_BR_GRAV                  0x00800000      /* Breathe Gravity */
+  {0, 25, 35, 0, 2},
+// RF4_BR_SHAR                  0x01000000      /* Breathe Shards */
+  {0, 15, 25, 0, 2},
+// RF4_BR_PLAS                  0x02000000      /* Breathe Plasma */
+  {0, 25, 30, 0, 2},
+// RF4_BR_WALL                  0x04000000      /* Breathe Force */
+  {0, 30, 40, 0, 2},
+// RF4_BR_MANA                  0x08000000      /* Breathe Mana */
+  {0, 35, 45, 0, 2},
+// RF4_BR_DISI                  0x10000000
+  {0, 50, 70, 0, 2},
+// RF4_BR_NUKE                  0x20000000
+  {0, 27, 40, 0, 2},
+// 0x40000000
+  {0, 0, 0, 0, 0},
+// RF4_BOULDER
+  {0, 2, 15, 0, 1},
+
+// RF5_BA_ACID                  0x00000001      /* Acid Ball */
+  {0, 8, 10, 0, 2},
+// RF5_BA_ELEC                  0x00000002      /* Elec Ball */
+  {0, 8, 10, 0, 2},
+// RF5_BA_FIRE                  0x00000004      /* Fire Ball */
+  {0, 8, 10, 0, 2},
+// RF5_BA_COLD                  0x00000008      /* Cold Ball */
+  {0, 8, 10, 0, 2},
+// RF5_BA_POIS                  0x00000010      /* Poison Ball */
+  {0, 11, 20, 0, 2},
+// RF5_BA_NETH                  0x00000020      /* Nether Ball */
+  {0, 25, 40, 0, 2},
+// RF5_BA_WATE                  0x00000040      /* Water Ball */
+  {0, 17, 30, 0, 2},
+// RF5_BA_MANA                  0x00000080      /* Mana Storm */
+  {0, 45, 50, 0, 2},
+// RF5_BA_DARK                  0x00000100      /* Darkness Storm */
+  {0, 20, 0, 0, 2},
+// RF5_DRAIN_MANA               0x00000200      /* Drain Mana */
+  {0, 0, 0, 0, 0},
+// RF5_MIND_BLAST               0x00000400      /* Blast Mind */
+  {0, 15, 13, 0, 2},
+// RF5_BRAIN_SMASH              0x00000800      /* Smash Brain */
+  {0, 25, 15, 0, 2},
+// RF5_CAUSE_1                  0x00001000      /* Cause Light Wound */
+  {0, 3, 15, 0, 1},
+// RF5_CAUSE_2                  0x00002000      /* Cause Serious Wound */
+  {0, 6, 20, 0, 1},
+// RF5_BA_NUKE                  0x00004000      /* Toxic Ball */
+  {0, 30, 30, 0, 2},
+// RF5_BA_CHAO                  0x00008000      /* Chaos Ball */
+  {0, 45, 40, 0, 2},
+// RF5_BO_ACID                  0x00010000      /* Acid Bolt */
+  {0, 4, 13, 0, 1},
+// RF5_BO_ELEC                  0x00020000      /* Elec Bolt (unused) */
+  {0, 3, 13, 0, 1},
+// RF5_BO_FIRE                  0x00040000      /* Fire Bolt */
+  {0, 4, 13, 0, 1},
+// RF5_BO_COLD                  0x00080000      /* Cold Bolt */
+  {0, 3, 13, 0, 1},
+// RF5_BO_POIS                  0x00100000      /* Poison Bolt (unused) */
+  {0, 5, 16, 0, 1},
+// RF5_BO_NETH                  0x00200000      /* Nether Bolt */
+  {0, 15, 20, 0, 1},
+// RF5_BO_WATE                  0x00400000      /* Water Bolt */
+  {0, 13, 18, 0, 1},
+// RF5_BO_MANA                  0x00800000      /* Mana Bolt */
+  {0, 20, 25, 0, 1},
+// RF5_BO_PLAS                  0x01000000      /* Plasma Bolt */
+  {0, 15, 18, 0, 1},
+// RF5_BO_ICEE                  0x02000000      /* Ice Bolt */
+  {0, 10, 15, 0, 1},
+// RF5_MISSILE                  0x04000000      /* Magic Missile */
+  {0, 2, 5, 0, 1},
+// RF5_SCARE                    0x08000000      /* Frighten Player */
+  {0, 3, 8, 0, 0},
+// RF5_BLIND                    0x10000000      /* Blind Player */
+  {0, 5, 8, 0, 0},
+// RF5_CONF                     0x20000000      /* Confuse Player */
+  {0, 5, 8, 0, 0},
+// RF5_SLOW                     0x40000000      /* Slow Player */
+  {0, 7, 10, 0, 0},
+// RF5_HOLD                     0x80000000      /* Paralyze Player (translates into forced monsleep) */
+  {0, 5, 10, 0, 0},
+
+// RF6_HASTE                    0x00000001      /* Speed self */
+  {0, 10, 20, 0, 0},
+// RF6_HAND_DOOM                0x00000002      /* Speed a lot (?) */
+  {0, 100, 80, 0, 0},
+// RF6_HEAL                     0x00000004      /* Heal self */
+  {0, 10, 20, 0, 0},
+// RF6_XXX2                     0x00000008      /* Heal a lot (?) */
+  {0, 0, 0, 0, 0},
+// RF6_BLINK                    0x00000010      /* Teleport Short */
+  {0, 5, 15, 0, 0},
+// RF6_TPORT                    0x00000020      /* Teleport Long */
+  {0, 20, 30, 0, 0},
+// RF6_XXX3                     0x00000040      /* Move to Player (?) */
+  {0, 0, 0, 0, 0},
+// RF6_XXX4                     0x00000080      /* Move to Monster (?) */
+  {0, 0, 0, 0, 0},
+// RF6_TELE_TO                  0x00000100      /* Move player to monster */
+  {0, 20, 30, 0, 0},
+// RF6_TELE_AWAY                0x00000200      /* Move player far away */
+  {0, 25, 30, 0, 0},
+// RF6_TELE_LEVEL               0x00000400      /* Move player vertically */
+  {0, 30, 60, 0, 0},
+// RF6_XXX5                     0x00000800      /* Move player (?) */
+  {0, 0, 0, 0, 0},
+// RF6_DARKNESS                 0x00001000      /* Create Darkness */
+  {0, 6, 8, 0, 0},
+// RF6_TRAPS                    0x00002000      /* Create Traps */
+  {0, 15, 25, 0, 0},
+// RF6_FORGET                   0x00004000      /* Cause amnesia */
+  {0, 25, 35, 0, 0},
+
+// RF0_BO_DISE			0x00000080
+  {0, 15, 20, 0, 1},
+// RF0_BA_DISE			0x00000100
+  {0, 30, 40, 0, 1},
+// RF0_BR_ICE			0x00040000
+  {0, 20, 27, 0, 2},
+// RF0_BR_WATER>		0x00080000
+  {0, 25, 30, 0, 2},
+]]
