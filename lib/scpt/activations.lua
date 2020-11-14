626b57aac2e54373c90b800694c4c3dee2ee3fab
C. Blue <mail@c-blue.de>
2020-11-14 07:51:45 +0100

    luarize info of trueart activations for client-side artifact lore.

---
+++ lib/scpt/activations.lua
@@ -0,0 +1,225 @@
+-- Activations for true artifact lore - C. Blue
+-- Sorted by name1
+
+true_art_activation = {
+--case ART_GALADRIEL:--1
+--damage details omitted
+{1, "illumination every 5..10+d10 turns"},
+--case ART_ELENDIL:--2
+{2, "magic mapping and light every 10..50+d25 turns"},
+--case ART_THRAIN:--3
+{3, "clairvoyance every 200..1000+d150 turns"},
+--case ART_CARLAMMAS:--4
+{4, "protection from evil every 25..225+d125 turns"},
+--case ART_INGWE:--5
+{5, "dispelling evil (lev x10 +0..500) every 50..300+d150 turns"},
+--case ART_FLAR:--7
+--[["dimension door every 100 turns",]]
+{7, "creating a gateway every 500..1000 turns"},
+--case ART_BARAHIR:--8
+{8, "dispelling small life (100..400) every 15..55+d55 turns"},
+--case ART_TULKAS:--9
+{9, "haste self (75+d50 turns) every 50..150+d100 turns"},
+--case ART_NARYA:--10
+{10, "a large fire ball (120..370) every 75..225+d75 turns"},
+--case ART_NENYA:--11
+{11, "a large frost ball (200..450) every 100..325+d125 turns"},
+--case ART_VILYA:--12
+{12, "a large lightning ball (250..500) every 100..425+d175 turns"},
+--case ART_POWER:--13
+{13, "powerful things every 450+d450 turns"},
+--case ART_ANCHOR:--14
+{14, "a Space-time barrier every 100..300+d20 turns"},
+--case ART_STONE_LORE:--15
+{15, "perilous identify every 4..10 turns (drains 20 mp)"},
+--case ART_RAZORBACK:--16
+{16, "lightning balls (8x150..600) every 300..1000 turns"},
+--case ART_BLADETURNER:--17
+{17, "Berserk rage and resistance every 200..400 turns"},
+--case ART_MELKOR:--18
+{18, "a darkness storm (150..300) every 20..100 turns"},
+--case ART_SOULKEEPER:--19
+{19, "healing (1000) every 222..888 turns"},
+--case ART_BELEGENNON:--22
+{22, "teleport every 2..10 turns"},
+--case ART_CELEBORN:--23
+{23, "genocide every 100..500 turns"},
+--case ART_CASPANION:--25
+{25, "door and trap destruction every 4..10 turns"},
+--case ART_MARDRA:--26
+{26, "banishing dragons (100..130) every 500..1000 turns"},
+--case ART_HURIN:--33
+{33, "berserker rage and +10 to speed (50) every 75..175+d75 turns"},
+--case ART_DOR:--39
+{39, "rays of fear in every direction every 3x(lev+10) turns"},
+--case ART_HOLHENNETH:--40
+{40, "detection every 15..55+d25 turns"},
+--case ART_GORLIM:--41
+{41, "rays of fear in every direction every 3x(lev+10) turns"},
+--case ART_GONDOR:--42
+{42, "healing (500) every 100..500 turns"},
+--case ART_NUMENOR:--43
+{43, "analyzing monsters every 50..300+d200 turns"},
+--case ART_COLLUIN:--44
+{44, "resistance (20+d20 turns) every 55..111 turns"},
+--case ART_HOLCOLLETH:--45
+{45, "sleep every 15..55 turns"},
+--case ART_THINGOL:--46
+{46, "recharging (60..100) an item every 20..70 turns"},
+--case ART_COLANNON:--48
+{48, "teleportation every 10..45 turns"},
+--case ART_LUTHIEN:--49
+{49, "restoring life levels every 100..450 turns"},
+--case ART_CAMMITHRIM:--53
+{53, "a magic missile (2..10d6) every 1..2 turns"},
+--case ART_PAURHACH:--54
+{54, "a fire bolt (9..29d8) every 2..8+d4 turns"},
+--case ART_PAURNIMMEN:--55
+{55, "a frost bolt (6..26d8) every 2..7+d3 turns"},
+--case ART_PAURAEGEN:--56
+{56, "a lightning bolt (4..24d8) every 2..6+d3 turns"},
+--case ART_PAURNEN:--57
+{57, "an acid bolt (5..25d8) every 2..5+d2 turns"},
+--case ART_FINGOLFIN:--59
+{59, "a magical arrow (150) every 15..90+d30 turns"},
+--case ART_FEANOR:--60
+{60, "haste self (20+d20 turns) every 50..200 turns"},
+--case ART_DAL:--61
+{61, "removing fear and curing poison every 5 turns"},
+--case ART_NARTHANC:--66
+{66, "a fire bolt (9..29d8) every 2..8+d8 turns"},
+--case ART_NIMTHANC:--67
+{67, "a frost bolt (6..26d8) every 2..7+d7 turns"},
+--case ART_DETHANC:--68
+{68, "a lightning bolt (4..24d8) every 2..6+d6 turns"},
+--case ART_RILIA:--69
+{69, "a stinking cloud (4..11) every 2..4+d4 turns"},
+--case ART_BELANGIL:--70
+{70, "a frost ball (48..108) every 2..5+d5 turns"},
+--case ART_ARUNRUTH:--72
+{72, "a frost bolt (12..27d8) every 100..500 turns"},
+--case ART_RINGIL:--82
+{82, "a frost ball (100..400) every 75..300 turns"},
+--case ART_ANDURIL:--83
+{83, "a fire ball (72..222) every 100..400 turns"},
+--case ART_ANGUIREL:--84
+{84, "a getaway every 15..35 turns"},
+--case ART_THEODEN:--93
+{93, "draining life (25%) every 100..400 turns"},
+--case ART_AEGLOS:--97
+{97, "a lightning ball (100..250) every 100..500 turns"},
+--case ART_OROME:--98
+{98, "stone-to-mud every 2..5 turns"},
+--case ART_EONWE:--102
+{102, "obliteration every 500..1000 turns"},
+--case ART_LOTHARANG:--104
+{104, "curing wounds (4d8..4d28) every 1..3+d3 turns"},
+--case ART_ULMO:--108
+{108, "teleporting away every 50..150 turns"},
+--case ART_AVAVIR:--109
+{109, "word of recall every 100..200 turns"},
+--case ART_DAWN:--110
+{110, "banishing undead (100..130) every 100..500+d100 turns"},
+--[[
+--case ART_ROBINTON:--137
+music_info[3].desc},
+--case ART_PIEMUR:--138
+music_info[9].desc},
+--case ART_MENOLLY:--139
+music_info[10].desc},
+]]
+--case ART_GROND:--111
+--111, "altering reality every 20000 turns"},
+--case ART_TOTILA:--112
+{112, "confusing a monster (lev +10..60) every 10..50 turns"},
+--case ART_FIRESTAR:--115
+{115, "a large fire ball (72..222) every 25..100 turns"},
+--case ART_TARATOL:--116
+{116, "haste self (20+d20 turns) every 30..100+d50 turns"},
+--case ART_ERIRIL:--119
+{119, "identifying every 2..10 turns"},
+--case ART_RUYIWANG:--120
+{120, "spinning around every 10..20+d5 turns"},
+--case ART_TURMIL:--122
+{122, "draining life (15%) every 20..70 turns"},
+--case ART_AXE_GOTHMOG:--123
+{123, "a fire ball (300..600) every 50..200+d200 turns"},
+--case ART_CUBRAGOL:--126
+{126, "fire branding of bolts every 222..999 turns"},
+--case ART_EREBOR:--140
+{140, "opening a secret passage every 50..200 turns"},
+--case ART_DRUEDAIN:--141
+{141, "detection every 49..99 turns"},
+--case ART_ROHAN:--142
+{142, "heroism, berserker rage, haste every 100..250+d50 turns"},
+--case ART_HELM:--143
+{143, "a sound ball (300..600) every 300 turns"},
+--case ART_BOROMIR:--144
+{144, "mass human summoning every 500..1000 turns"},
+--case ART_ERU:--147
+{147, "healing (1000), curing every 250..500 turns"},
+--case ART_GANDALF:--127
+{127, "restoring mana every 222..666 turns"},
+--case ART_ORCHAST:--156
+{156, "detecting orcs every 10 turns"},
+--case ART_NIGHT:--157
+{157, "draining life (3*7%) every 50..250 turns"},
+--case ART_NATUREBANE:--158
+{158, "dispelling monsters (300) every 100..200+d200 turns"},
+--case ART_KNOWLEDGE:--160
+{160, "whispers from beyond 100..200+d100 turns"},
+--case ART_LEBOHAUM:--165
+{165, "singing a cheerful song every 30 turns"},
+--case ART_MEDIATOR:--166
+{166, "breathing elements (300..600), berserk, resistance every 100..400 turns"},
+--case ART_HIMRING:--167
+{167, "protection from evil (dur 30+d15) every 75..225+d125 turns"},
+--case ART_GILGALAD:--169
+{169, "starlight (75..225) every 15..75+d50 turns"},
+--case ART_CELEBRIMBOR:--170
+{170, "temporary ESP (dur 20+d20) every 25..50+d25 turns"},
+--case ART_UMBAR:--171
+{171, "a magic arrow (10..30d10) every 5..20+d10 turns"},
+--case ART_NAIN:--174
+{174, "stone-to-mud every 4..7+d5 turns"},
+--case ART_FUNDIN:--175
+{175, "dispelling evil (lev x8 +0..400) every 20..100+d50 turns"},
+--case ART_HARADRIM:--176
+{176, "berserk strength every 15..50+d40 turns"},
+--case ART_SKULLCLEAVER:--177
+{177, "destruction every 100..200+d200 turns"},
+--case ART_EOL:--178
+{178, "a mana bolt (9..29d8) 2..7+d3 turns"},
+--case ART_UNDEATH:--200
+{200 "darkness and ruination every 5..10+d10 turns"},
+--case ART_PALANTIR:--202
+{202, "clairvoyance every 200..1000+d150 turns"},
+--case ART_PHASING:--203
+{203, "opening the final gate to the shores of Valinor every 1000 turns"},
+--case ART_ELESSAR:--206
+{206, "healing and curing black breath every 100..200 turns"},
+--case ART_EVENSTAR:--207
+{207, "restoration every 50..150 turns"},
+--case ART_PALANTIR_ITHIL:--208
+{208, "clairvoyance every 200..1000+d150 turns"},
+--case ART_BILBO:--214
+{214, "destroying doors and traps every 5..30+d10 turns"},
+--case ART_SOULCURE:--232
+{232, "holy prayer (+20 AC) every 50..150+d100 turns"},
+--case ART_AMUGROM:--233
+{233, "temporary resistance boost every 50..150+d50 turns"},
+--case ART_HELLFIRE:--236
+{236, "invoking hellfire (400..600) every 30+d10 turns"},
+--case ART_SPIRITSHARD:--244
+{244, "turning into a wraith every 50..100+d50 turns"},
+--case ART_GOGGLES_DM:--261
+{261, "identifying possessions"},
+--case ART_HAVOC:--274
+{274, "invoking a force bolt (8..24d8) every 1+d2 turns"},
+--case ART_SMASHER:--276
+{276, "destroying doors every 10..15+d3 turns"},
+--case ART_FISTS:--278
+{278, "hellfire brand every 150..350+d50 turns"},
+--case ART_WARPSPEAR:--280
+{280, "teleport-to every 15..40+d5 turns"},
+}
