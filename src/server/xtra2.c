033d7a16fa34d330bfcb593195a9781299a1f40a
TomeNET <no-addr@tomenet.net>
2021-03-26 13:26:16 +0100

    melee brand hack to avoid message spam for non-self-applied brands that fail.

--- src/server/xtra2.c
+++ src/server/xtra2.c
@@ -735,8 +735,10 @@ bool set_melee_brand(int Ind, int v, u16b t, int p) {
 		strcpy(weapons, "\377wYour weapon");
 		plural = FALSE;
 	} else {
-		if (v) msg_print(Ind, "You are not wielding any melee weapons to brand."); /* failure */
-		else { /* Shut */
+		if (v) {
+			/* Hack: p == 9 is a marker that this buff wasn't applied by ourself. Prevent msg-spam in this case. */
+			if (p != 9) msg_print(Ind, "You are not wielding any melee weapons to brand."); /* failure */
+		} else { /* Shut */
 			p_ptr->melee_brand = 0;
 			p_ptr->melee_brand_t = 0;
 			p_ptr->melee_brand_d = 0;
