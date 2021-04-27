6ed2629812bb346ff5c9cea9dd532257f574674f
C. Blue <mail@c-blue.de>
2021-04-27 19:34:03 +0200

    corrected diz for TR6_RETURNING

--- src/server/object1.c
+++ src/server/object1.c
@@ -5481,7 +5481,7 @@ bool identify_combo_aux(int Ind, object_type *o_ptr, bool full, int item) {
 			fprintf(fff, "It fires missiles excessively fast.\n");
 	}
 
-	if (f6 & TR6_RETURNING) fprintf(fff, "It returns to you when thrown.\n");
+	if (f6 & TR6_RETURNING) fprintf(fff, "It returns to you when thrown while equipped.\n");
 
 	if (f4 & (TR4_EASY_USE))
 		fprintf(fff, "It is especially easy to activate.\n");
