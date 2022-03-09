622b87f188de7ff268fcf3a2c9013c2150644ba3
C. Blue <mail@c-blue.de>
2022-03-09 01:20:00 +0100

    rotate seasons twice real-time, keeping christmas aligned with winter on northern hemisphere

--- lib/scpt/custom.lua
+++ lib/scpt/custom.lua
@@ -45,24 +45,76 @@ function playloop_startup(timestamp, h, m, s, dwd, dd, dm, dy)
 	--[[automatically fix season if required, for example if server
 	    was down for 24+ hours while a season change date occured.
 	    see cron_24h() for details and to keep values synchronized.]]
-	if (season ~= 0) then
-		if (dm >= 3 and dm <= 5) then
-			lua_season_change(0, 0)
+	if false then
+		-- seasons follow northern hemisphere seasons in real-time
+
+		if (season ~= 0) then -- spring
+			-- march + april + may
+			if (dm >= 3 and dm <= 5) then
+				lua_season_change(0, 0)
+			end
 		end
-	end
-	if (season ~= 1) then
-		if ((dm >= 6 and dm <= 8) or (dm == 9 and dd < 23)) then
-			lua_season_change(1, 0)
+		if (season ~= 1) then -- summer
+			-- june + july + aug + 22 days
+			if ((dm >= 6 and dm <= 8) or (dm == 9 and dd < 23)) then
+				lua_season_change(1, 0)
+			end
 		end
-	end
-	if (season ~= 2) then
-		if ((dm == 9 and dd >= 23) or (dm >= 10 and dm <= 11) or (dm == 12 and dd < 22)) then
-			lua_season_change(2, 0)
+		if (season ~= 2) then -- autumn
+			-- 8 days + oct + nov + 21 days
+			if ((dm == 9 and dd >= 23) or (dm >= 10 and dm <= 11) or (dm == 12 and dd < 22)) then
+				lua_season_change(2, 0)
+			end
 		end
-	end
-	if (season ~= 3) then
-		if ((dm == 12 and dd >= 22) or (dm < 3)) then
-			lua_season_change(3, 0)
+		if (season ~= 3) then -- winter
+			-- 10 days + jan + feb
+			if ((dm == 12 and dd >= 22) or (dm < 3)) then
+				lua_season_change(3, 0)
+			end
+		end
+	else
+		-- seasons run at 3x real-time, and specifically aligning northern-hemisphere-style with christmas during winter
+		-- additionally aligned it so that summer solstice / winter solstice are the final days of each cycle ^^
+
+		if (season ~= 0) then -- spring
+			-- 7 days + feb + 8 days (march)
+			if ((dm == 1 and dd >= 25) or (dm == 2) or (dm == 3 and dd <= 8)) then
+				lua_season_change(0, 0)
+			end
+			-- 7 days + aug + 8 days (sept)
+			if ((dm == 7 and dd >= 25) or (dm == 8) or (dm == 9 and dd <= 8)) then
+				lua_season_change(0, 0)
+			end
+		end
+		if (season ~= 1) then -- summer
+			-- 22 days + april + 4 days (may)
+			if ((dm == 3 and dd >= 9) or (dm == 4) or (dm == 5 and dd <= 4)) then
+				lua_season_change(1, 0)
+			end
+			-- 21 days + oct + 4 days (nov)
+			if ((dm == 9 and dd >= 9) or (dm == 10) or (dm == 11 and dd <= 4)) then
+				lua_season_change(1, 0)
+			end
+		end
+		if (season ~= 2) then -- autumn
+			-- 26 days + 21 days (june)
+			if ((dm == 5 and dd >= 5) or (dm == 6 and dd <= 21)) then
+				lua_season_change(2, 0)
+			end
+			-- 25 days + 21 days (dec)
+			if ((dm == 11 and dd >= 5) or (dm == 12 and dd <= 21)) then
+				lua_season_change(2, 0)
+			end
+		end
+		if (season ~= 3) then -- winter
+			-- 10 days + 24 days (july)
+			if ((dm == 6 and dd >= 22) or (dm == 7 and dd <= 24)) then
+				lua_season_change(3, 0)
+			end
+			-- 10 days + 24 days (january)
+			if ((dm == 12 and dd >= 22) or (dm == 1 and dd <= 24)) then
+				lua_season_change(3, 0)
+			end
 		end
 	end
 
@@ -137,12 +189,28 @@ function cron_24h(timestamp, h, m, s, dwd, dd, dm, dy)
 	lua_s_print(timestamp.."_CRON24H_"..h..":"..m..":"..s.."-"..dy.."/"..dm.."/"..dd.."("..dwd..")\n")
 
 	--[[season changes (averaged, may in reality vary +/- 1 day:
-	    spring 20.march, summer 21.june,
-	    autumn 23. september, winter 21. december ]]
-	if (dd == 1 and dm == 3) then lua_season_change(0, 0) end
-	if (dd == 1 and dm == 6) then lua_season_change(1, 0) end
-	if (dd == 23 and dm == 9) then lua_season_change(2, 0) end
-	if (dd == 22 and dm == 12) then lua_season_change(3, 0) end
+	    spring 1. march, summer 1. june,
+	    autumn 23. september, winter 22. december ]]
+	if false then
+		-- seasons follow northern hemisphere seasons in real-time
+
+		if (dd == 1 and dm == 3) then lua_season_change(0, 0) end -- spring
+		if (dd == 1 and dm == 6) then lua_season_change(1, 0) end -- summer
+		if (dd == 23 and dm == 9) then lua_season_change(2, 0) end -- autumn
+		if (dd == 22 and dm == 12) then lua_season_change(3, 0) end -- winter
+	else
+		-- seasons run at 3x real-time, and specifically aligning northern-hemisphere-style with christmas during winter
+		-- additionally aligned it so that summer solstice / winter solstice are the final days of each cycle ^^
+
+		if (dd == 25 and dm == 1) then lua_season_change(0, 0) end -- spring A
+		if (dd == 25 and dm == 7) then lua_season_change(0, 0) end -- spring B
+		if (dd == 9 and dm == 3) then lua_season_change(1, 0) end -- summer A
+		if (dd == 9 and dm == 9) then lua_season_change(1, 0) end -- summer B
+		if (dd == 5 and dm == 5) then lua_season_change(2, 0) end -- autumn A
+		if (dd == 5 and dm == 11) then lua_season_change(2, 0) end -- autumn B
+		if (dd == 22 and dm == 6) then lua_season_change(3, 0) end -- winter B
+		if (dd == 22 and dm == 12) then lua_season_change(3, 0) end -- winter A
+	end
 
 	--regrow fields (gardens) sometimes :/
 	if mod(dd, 4) == rand_int(4) then
