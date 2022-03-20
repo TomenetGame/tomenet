8d0b6706da2ceb349f7e4cc38f5bfd22a40b8818
C. Blue <mail@c-blue.de>
2022-03-20 12:58:19 +0100

    note that quests.lua is purely server-side

--- lib/scpt/quests.lua
+++ lib/scpt/quests.lua
@@ -1,3 +1,4 @@
+-- Server-side use only.
 -- Purpose of this file is to provide special lua functions that are called by the
 -- quests framework in q_info.txt. As convention to keep things clean, these should
 -- all be named 'quest_' and end on the 'internal quest codename'.  - C. Blue
