fc504482b60cb919b69f07b71b7c6b9c59c4985a
C. Blue <mail@c-blue.de>
2022-09-22 20:11:04 +0200

    disabled EXTENDED_BG_COLOURS as it causes daylight-shading palette_animation to no longer work

--- src/common/defines-features.h
+++ src/common/defines-features.h
@@ -192,8 +192,9 @@
    Note: This is highly EXPERIMENTAL and not even implemented atm.,
          the only thing that works is proof of concept code that displays
          rain in alternating colours, TERM_ORANGE and (newly added for this) TERMX_BLUE.
-         2022-03-10 - reenabling this, not for rain but for floor background 'test'. */
-#define EXTENDED_BG_COLOURS
+         2022-03-10 - reenabling this, not for rain but for floor background 'test'.
+   -- !!! CURRENTLY CAUSING A BUG: palette_animation no longer works (daylight-shading) -- */
+//#define EXTENDED_BG_COLOURS
 
 /* better chance for non-low +hit,+dam on randart melee weapons and boomerangs */
 #define RANDART_WEAPON_BUFF
