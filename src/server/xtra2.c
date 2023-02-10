3e8fb05aab8e69c16369bcee11d5a1059a965270
C. Blue <mail@c-blue.de>
2023-02-10 18:11:45 +0100

    formatting

--- src/server/xtra2.c
+++ src/server/xtra2.c
@@ -8199,6 +8199,7 @@ void kill_house_contents(house_type *h_ptr) {
 		/* Log removal of player store items */
 		if (o_ptr->note && strstr(quark_str(o_ptr->note), "@S")) {
 			char o_name[ONAME_LEN];//, p_name[NAME_LEN];
+
 			object_desc(0, o_name, o_ptr, TRUE, 3);
 			//s_printf("PLAYER_STORE_REMOVED (khouse): %s - %s (%d,%d,%d; %d,%d).\n",
 			s_printf("PLAYER_STORE_REMOVED (khouse): %s (%d,%d,%d; %d,%d; %d).\n",
