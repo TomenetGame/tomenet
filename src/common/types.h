211d893850e66863846c3c18374d62b0b6a4c0c4
C. Blue <mail@c-blue.de>
2023-02-15 06:54:35 +0100

    (formatting)

--- src/common/types.h
+++ src/common/types.h
@@ -3512,7 +3512,7 @@ struct player_type {
 #ifdef AUTO_RET_CMD
 	byte autoret_base;		/* set auto-retaliation via command instead of inscription - used for martial arts and generic extra stuff (no sleeping monsters) */
 	u16b autoret_mu;		/* set auto-retaliation via command instead of inscription - used for mimics and runemasters ie magic that doesn't use items that could be inscribed */
- #endif
+#endif
 	bool shoot_till_kill, shooty_till_kill, shooting_till_kill; /* Shoot a target until it's dead, like a ranged 'auto-retaliator' - C. Blue */
 	int shoot_till_kill_book, shoot_till_kill_spell, shoot_till_kill_mimic; //and there's shoot_till_kill_rcraft too
 	int shoot_till_kill_wand, shoot_till_kill_rod;
