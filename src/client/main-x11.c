10cc5beef4fd167bc9cd0f16e47dfe530f9ce92f
C. Blue <mail@c-blue.de>
2023-03-10 00:16:26 +0100

    x11: fix ENABLE_SHIFT_SPECIALKEYS handling for the special keys (ESC, RETURN, TAB, DEL/BKSP).

--- src/client/main-x11.c
+++ src/client/main-x11.c
@@ -1306,9 +1306,9 @@ static errr Infowin_set_class_hint(cptr name) {
 static void react_keypress(XEvent *xev) {
 	int i, n, mc, ms, mo, mx;
 
-#ifndef ENABLE_SHIFT_SPECIALKEYS
+//#ifndef ENABLE_SHIFT_SPECIALKEYS
 	uint ks1;
-#endif
+//#endif
 
 	XKeyEvent *ev = (XKeyEvent*)(xev);
 
@@ -1324,10 +1324,10 @@ static void react_keypress(XEvent *xev) {
 	/* Terminate */
 	buf[n] = '\0';
 
-#ifndef ENABLE_SHIFT_SPECIALKEYS
+//#ifndef ENABLE_SHIFT_SPECIALKEYS
 	/* Hack -- convert into an unsigned int */
 	ks1 = (uint)(ks);
-#endif
+//#endif
 
 	/* Extract four "modifier flags" */
 	mc = (ev->state & ControlMask) ? TRUE : FALSE;
@@ -1352,8 +1352,9 @@ static void react_keypress(XEvent *xev) {
 		return;
 	}
 
-
-#ifndef ENABLE_SHIFT_SPECIALKEYS /* Totally no need for this code, even if this feat weren't defined? ^^' */
+#if 0 /* 0'ed, to Reinstatiate original code, read next comment for details: */
+ /* Wrong ^! We need this code, to get clean keys returned from get_macro_trigger(), when pressing ESC key! (No idea if there are more cases than this; reinstatiating this code:) */
+ #ifndef ENABLE_SHIFT_SPECIALKEYS /* Totally no need for this code, even if this feat weren't defined? ^^' */
 	/* Handle a few standard keys */
 	switch (ks1) {
 		case XK_Escape:
@@ -1367,6 +1368,47 @@ static void react_keypress(XEvent *xev) {
 
 		case XK_Delete:
 		case XK_BackSpace:
+		Term_keypress('\010'); return;
+	}
+ #endif
+#else
+	/* Handle a few standard keys */
+	switch (ks1) {
+		case XK_Escape:
+ #ifdef ENABLE_SHIFT_SPECIALKEYS
+		/* As we're shortcutting the whole key-evaluation with our early 'return' here, set the shiftkey flags manually */
+		if (ms) inkey_shift_special |= 0x1;
+		if (mc) inkey_shift_special |= 0x2;
+		if (mo) inkey_shift_special |= 0x4;
+ #endif
+		Term_keypress(ESCAPE); return;
+
+		case XK_Return:
+ #ifdef ENABLE_SHIFT_SPECIALKEYS
+		/* As we're shortcutting the whole key-evaluation with our early 'return' here, set the shiftkey flags manually */
+		if (ms) inkey_shift_special |= 0x1;
+		if (mc) inkey_shift_special |= 0x2;
+		if (mo) inkey_shift_special |= 0x4;
+ #endif
+		Term_keypress('\r'); return;
+
+		case XK_Tab:
+ #ifdef ENABLE_SHIFT_SPECIALKEYS
+		/* As we're shortcutting the whole key-evaluation with our early 'return' here, set the shiftkey flags manually */
+		if (ms) inkey_shift_special |= 0x1;
+		if (mc) inkey_shift_special |= 0x2;
+		if (mo) inkey_shift_special |= 0x4;
+ #endif
+		Term_keypress('\t'); return;
+
+		case XK_Delete:
+		case XK_BackSpace:
+ #ifdef ENABLE_SHIFT_SPECIALKEYS
+		/* As we're shortcutting the whole key-evaluation with our early 'return' here, set the shiftkey flags manually */
+		if (ms) inkey_shift_special |= 0x1;
+		if (mc) inkey_shift_special |= 0x2;
+		if (mo) inkey_shift_special |= 0x4;
+ #endif
 		Term_keypress('\010'); return;
 	}
 #endif
