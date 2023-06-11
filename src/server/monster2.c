9ba91c2fc6476154b1d0114a32e34c57c3cf2a38
C. Blue <mail@c-blue.de>
2023-06-11 05:02:25 +0200

    (oups)

--- src/server/monster2.c
+++ src/server/monster2.c
@@ -6659,7 +6659,7 @@ else s_printf("\n");
 	}
 #endif
 
-#ifdef _SIMPLE_RI_MIRROR_CHECKFORSPELLS
+#ifdef SIMPLE_RI_MIRROR_CHECKFORSPELLS
 	if (check_for_spell(p_ptr, "HEALINGCLOUD_I") || check_for_spell(p_ptr, "HEALINGCLOUD_II") || check_for_spell(p_ptr, "HEALINGCLOUD_III")) { r_ptr->flags6 |= RF6_HEAL; magicness++; )
 	//r_ptr->flags9 |= RF9_RES_POIS; } --nah
 	/* Note: Focus is currently ignored, so player gains great +hit chance advantage. */
