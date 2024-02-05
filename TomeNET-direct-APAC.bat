3044df3a41aa83fbc3d5013a6d6bccad2bc3fd4b
C. Blue <mail@c-blue.de>
2024-02-05 12:26:27 +0100

    oups, symlinks -> actual files

--- TomeNET-direct-APAC.bat
+++ TomeNET-direct-APAC.bat
@@ -1,1 +1,5 @@
-/home/cblue/Coding/TomeNET/TomeNET/TomeNET-direct-APAC.bat
+REM Windows batch script to connect directly to the Asia-Pacific server, without the need for the Meta server or DNS resolution.
+
+@echo off
+TomeNET.exe 92.38.178.77
+REM TomeNET.exe apac.tomenet.eu
--- TomeNET-direct-EU.bat
+++ TomeNET-direct-EU.bat
@@ -1,1 +1,5 @@
-/home/cblue/Coding/TomeNET/TomeNET/TomeNET-direct-EU.bat
+REM Windows batch script to connect directly to the Europe server, without the need for the Meta server or DNS resolution.
+
+@echo off
+TomeNET.exe 37.187.75.24
+REM TomeNET.exe europe.tomenet.eu
