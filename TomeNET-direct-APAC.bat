4a70709e4e288fc5bc5cbe1531f045e7dbd01f23
C. Blue <mail@c-blue.de>
2024-02-05 12:27:37 +0100

    echo off first

--- TomeNET-direct-APAC.bat
+++ TomeNET-direct-APAC.bat
@@ -1,5 +1,5 @@
+@echo off
 REM Windows batch script to connect directly to the Asia-Pacific server, without the need for the Meta server or DNS resolution.
 
-@echo off
 TomeNET.exe 92.38.178.77
 REM TomeNET.exe apac.tomenet.eu
--- TomeNET-direct-EU.bat
+++ TomeNET-direct-EU.bat
@@ -1,5 +1,5 @@
+@echo off
 REM Windows batch script to connect directly to the Europe server, without the need for the Meta server or DNS resolution.
 
-@echo off
 TomeNET.exe 37.187.75.24
 REM TomeNET.exe europe.tomenet.eu
