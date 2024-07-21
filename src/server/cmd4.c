dec90cc1b0b4b6fe8596052ab5b3498471413b89
C. Blue <mail@c-blue.de>
2024-07-21 02:57:41 +0200

    respite hub -> refuge hub

--- src/server/cmd4.c
+++ src/server/cmd4.c
@@ -2935,10 +2935,10 @@ void do_cmd_check_server_settings(int Ind) {
 
 #ifdef IDDC_REFUGES
 	if (p_ptr->depth_in_feet) {
-		fprintf(fff, "\nThe Ironman Deep Dive Challenge has respite hubs every -%3dft,", 50 * IDDC_REFUGE_INTERVAL);
+		fprintf(fff, "\nThe Ironman Deep Dive Challenge has refuge hubs every -%3dft,", 50 * IDDC_REFUGE_INTERVAL);
 		fprintf(fff, "\n within each -1000ft interval.\n");
 	} else {
-		fprintf(fff, "\nThe Ironman Deep Dive Challenge has respite hubs every %d floors,", IDDC_REFUGE_INTERVAL);
+		fprintf(fff, "\nThe Ironman Deep Dive Challenge has refuge hubs every %d floors,", IDDC_REFUGE_INTERVAL);
 		fprintf(fff, "\n within each 20-floor interval.\n");
 	}
 #endif
