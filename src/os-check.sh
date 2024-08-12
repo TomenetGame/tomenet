3a5cd34a961c755cf396aa01eee4acc5339b3a34
C. Blue <mail@c-blue.de>
2024-08-12 15:19:54 +0200

    (oops)

--- src/os-check.sh
+++ src/os-check.sh
@@ -1,8 +1,8 @@
 # Helper script to determine less common OS - C. Blue
 
 if [ "$ANDROID_ROOT" != "" ] || [ "`uname -r | grep -i android`" != "" ]; then echo "-DANDROID" # practically implies GCU atm
-elif [ "`uname -s | grep -i iPhone`" != "" ]; then echo "-DIOS"
-elif [ "`uname -s | grep -i iPad`" != "" ]; then echo "-DIOS"
+elif [ "`uname -s | grep -i iPhone`" != "" ]; then echo "-DIPHONE"
+elif [ "`uname -s | grep -i iPad`" != "" ]; then echo "-DIPAD"
 elif [ "`uname -a |  grep -i Darwin`" != "" ]; then echo "-DOSX" # practically redudant as it is (should be) set in the makefile.osx
 elif [ "`uname -s |  grep -i Linux`" != "" ]; then echo "-DLINUX" # a dummy, as we use X11/GCU for this as specializations anyway
 else echo "-DANY_`uname -s`"
