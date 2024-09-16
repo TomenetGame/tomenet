fd3a5d3d7be4f1fc6ac6bee6d3a64343dba348e1
C. Blue <mail@c-blue.de>
2024-09-16 19:35:17 +0200

    fixed gold from chests for IDDC_AUTOSPLIT_GOLD

--- src/server/object2.c
+++ src/server/object2.c
@@ -9899,13 +9899,19 @@ void place_gold(int Ind, struct worldpos *wpos, int y, int x, int mult, int bonu
 	}
 
 	if (Ind) {
-		forge.killer = Players[Ind]->id;
+		player_type *p_ptr = Players[Ind];
+
+		forge.killer = p_ptr->id;
+#ifdef IDDC_AUTOSPLIT_GOLD
+		if (opening_chest && !(p_ptr->party && in_irondeepdive(wpos))) {
+#else
 		if (opening_chest) {
+#endif
 			/* Gold in the IDDC is untradable, add special marker to circumvent this for money from chests.
 			   We still need to set its trade-flags here though to keep sharing it restricted to the party, which is easier than just not pre-owning it! */
 			forge.owner = forge.killer;
-			forge.mode = Players[Ind]->mode;
-			forge.iron_trade = Players[Ind]->iron_trade; /* gold cannot be traded in IDDC anyway, so this has no effect.. */
+			forge.mode = p_ptr->mode;
+			forge.iron_trade = p_ptr->iron_trade; /* gold cannot be traded in IDDC anyway, so this has no effect.. */
 			forge.iron_turn = opening_chest;
 			if (forge.tval == TV_GOLD) forge.xtra3 = 1; /* mark as exception for trading in the IDDC */
 		}
