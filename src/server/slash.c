d9e3e3c342fe800b8f04607bf42d6f0134a5c24d
C. Blue <mail@c-blue.de>
2024-10-14 14:07:22 +0200

    /rest command: removed shortcut /re due to collisions

--- src/server/slash.c
+++ src/server/slash.c
@@ -5942,7 +5942,7 @@ void do_slash_cmd(int Ind, char *message, char *message_u) {
 
 			p_ptr->energy -= level_speed(&p_ptr->wpos);
 			return;
-		} else if (prefix(messagelc, "/re")) {// || prefix(messagelc, "/rest")) { /* Rest [for n turns] */
+		} else if (prefix(messagelc, "/rest")) { /* Rest [for n turns] */
 			if (tk && (k <= 0 || k >= 10000)) {
 				msg_print(Ind, "\377yUsage: /rest [1..10000 turns]");
 				return;
