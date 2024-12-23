ac34f91b16fab8877498265704a1608de117f5f9
C. Blue <mail@c-blue.de>
2024-12-23 03:17:45 +0100

    fixed duplicate font entries from accumulating runs of this script on each client startup, which would eventually lead to max font path string size exceeding error 51

--- lib/xtra/posix_extra_fonts/set-font-path.sh
+++ lib/xtra/posix_extra_fonts/set-font-path.sh
@@ -2,11 +2,20 @@
 
 # Add subdirectories that contain a fonts.dir file to X server font path by calling "xset +fp" - mikaelh
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+#SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+# Split up the above line cause it gave me bad syntax highlighting in silyl mc ^^' - C. Blue
+DIR=$(dirname "$0")
+SCRIPTPATH="$( cd -- "$DIR" >/dev/null 2>&1 ; pwd -P )"
+
 cd "${SCRIPTPATH}"
 
 for i in */fonts.dir; do
 	SUBDIR=$(dirname "${i}"})
 	#echo xset +fp "${SCRIPTPATH}/${SUBDIR}"
+
+	# Hack: remove [multiple] duplicates of this font path, that were potentially added during previous runs - C. Blue
+	# Otherwise we'd eventually exceed the max string size of the font path and get an error 51:
+	# X Error of failed request: BadLength (poly request too large or internal Xlib length error)
+	xset -fp "${SCRIPTPATH}/${SUBDIR}"
 	xset +fp "${SCRIPTPATH}/${SUBDIR}"
 done
