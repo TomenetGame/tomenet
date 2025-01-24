ab998c0b0b5fc9d8c36b118cf0379a01f60c30b8
C. Blue <mail@c-blue.de>
2025-01-24 23:43:25 +0100

    --- version bump to 4.9.3 ---

--- src/common/defines.h
+++ src/common/defines.h
@@ -40,10 +40,10 @@
 /* MAJOR/MINOR/PATCH version should be 0-15. BUILD == 1 means 'test build' */
 #define VERSION_MAJOR		4
 #define VERSION_MINOR		9
-#define VERSION_PATCH		2
-#define VERSION_EXTRA		1
+#define VERSION_PATCH		3
+#define VERSION_EXTRA		0
 #define VERSION_BRANCH		0
-#define VERSION_BUILD		1
+#define VERSION_BUILD		0
 
 /* MAJOR/MINOR/PATCH version that counts as 'latest' (should be 0-15).
    If a player is online with a version > this && <= current version (VERSION_)
@@ -51,7 +51,7 @@
    with a 'T' marker which is visible only to admins.*/
 #define VERSION_MAJOR_LATEST	4
 #define VERSION_MINOR_LATEST	9
-#define VERSION_PATCH_LATEST	2
+#define VERSION_PATCH_LATEST	3
 #define VERSION_EXTRA_LATEST	0
 #define VERSION_BRANCH_LATEST	0
 #define VERSION_BUILD_LATEST	0
@@ -59,10 +59,10 @@
 /* maximum MAJOR/MINOR/PATCH version that counts as 'outdated' (should be 0-15). */
 #define VERSION_MAJOR_OUTDATED	4
 #define VERSION_MINOR_OUTDATED	9
-#define VERSION_PATCH_OUTDATED	1
+#define VERSION_PATCH_OUTDATED	2
 #define VERSION_EXTRA_OUTDATED	0
 #define VERSION_BRANCH_OUTDATED	0
-#define VERSION_BUILD_OUTDATED	2 /* should always be 1 to invalidate previous 'test' versions */
+#define VERSION_BUILD_OUTDATED	1 /* should always be 1 (or higher) to invalidate previous 'test' versions */
 
 /* Server release version tag (such as "a", "b" etc), or empty if none:
    Minimum client version tag required to "play 100%". */
