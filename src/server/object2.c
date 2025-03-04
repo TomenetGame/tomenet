2a1153c4675f1aa75f1df005446787a1e0d164c0
C. Blue <mail@c-blue.de>
2025-03-04 14:21:18 +0100

    Predefined macros: F2 starter spell: Maiar prioritize the granted Power Bolt scroll.

--- src/server/object2.c
+++ src/server/object2.c
@@ -12015,11 +12015,14 @@ s16b inven_carry(int Ind, object_type *o_ptr) {
 
 /* Helper function for character birth: Equip starter items automatically. */
 void inven_carry_equip(int Ind, object_type *o_ptr) {
+	player_type *p_ptr = Players[Ind];
 	int item;
 
 	/* Inscribe basic spell for use with F1 predefined macro;
-	   assuming that noone receives more than 1 TV_BOOK item, so inscriptions won't collide. */
-	if (o_ptr->tval == TV_BOOK) o_ptr->note = quark_add("@m1");
+	   Maiar can receive more than 1 TV_BOOK item as they are granted a Power Bolt scroll racially,
+	   so for them ignore other books to avoid collision of multiple @m1 inscriptions. */
+	if (o_ptr->tval == TV_BOOK &&
+	    (p_ptr->prace != RACE_MAIA || o_ptr->pval == __lua_POWERBOLT)) o_ptr->note = quark_add("@m1");
 
 	/* First, put it into the inventory */
 	item = inven_carry(Ind, o_ptr);
@@ -12029,16 +12032,16 @@ void inven_carry_equip(int Ind, object_type *o_ptr) {
 
 	if (is_ammo(o_ptr->tval)) {
 		/* Don't put ammo into the quiver if it does't match an already equipped ranged weapon! */
-		if (Players[Ind]->inventory[INVEN_BOW].tval) {
+		if (p_ptr->inventory[INVEN_BOW].tval) {
 			switch (o_ptr->tval) { /* No need to check INVEN_BOW for tval actually */
 			case TV_SHOT:
-				if (Players[Ind]->inventory[INVEN_BOW].sval != SV_SLING) return;
+				if (p_ptr->inventory[INVEN_BOW].sval != SV_SLING) return;
 				break;
 			case TV_ARROW:
-				if (Players[Ind]->inventory[INVEN_BOW].sval != SV_SHORT_BOW && Players[Ind]->inventory[INVEN_BOW].sval != SV_LONG_BOW) return;
+				if (p_ptr->inventory[INVEN_BOW].sval != SV_SHORT_BOW && p_ptr->inventory[INVEN_BOW].sval != SV_LONG_BOW) return;
 				break;
 			case TV_BOLT:
-				if (Players[Ind]->inventory[INVEN_BOW].sval != SV_LIGHT_XBOW && Players[Ind]->inventory[INVEN_BOW].sval != SV_HEAVY_XBOW) return;
+				if (p_ptr->inventory[INVEN_BOW].sval != SV_LIGHT_XBOW && p_ptr->inventory[INVEN_BOW].sval != SV_HEAVY_XBOW) return;
 				break;
 			}
 		}
@@ -12051,7 +12054,7 @@ void inven_carry_equip(int Ind, object_type *o_ptr) {
 		suppress_message = FALSE;
 
 		/* make torches somewhat 'used' */
-		if (o_ptr->tval == TV_LITE && o_ptr->sval == SV_LITE_TORCH) Players[Ind]->inventory[INVEN_LITE].timeout -= rand_int(FUEL_TORCH / 10);
+		if (o_ptr->tval == TV_LITE && o_ptr->sval == SV_LITE_TORCH) p_ptr->inventory[INVEN_LITE].timeout -= rand_int(FUEL_TORCH / 10);
 	}
 }
 
