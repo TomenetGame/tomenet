e15169ccc21f21da35718b8d1f02586ac1f78eef
C. Blue <mail@c-blue.de>
2025-03-27 22:04:40 +0100

    jail-patience fix

--- src/server/util.c
+++ src/server/util.c
@@ -10161,9 +10161,10 @@ void grid_affects_player(int Ind, int ox, int oy) {
 	if (ox != -1 && (zcave[oy][ox].info & CAVE_STCK) && !(zcave[y][x].info & CAVE_STCK)) {
 		/* If we don't get insta-kicked out of jail (JAIL_KICK),
 		   we might actually be walking out of a jail, not out of a vault */
-		if (zcave[oy][ox].info & CAVE_JAIL)
+		if (zcave[oy][ox].info & CAVE_JAIL) {
 			msg_print(Ind, "\377sFresh air greets you as you leave the prison.");
-		else
+			p_ptr->tim_jail_delay = 0;
+		} else
 			msg_print(Ind, "\377sFresh air greets you as you leave the vault.");
 		p_ptr->redraw |= PR_DEPTH; /* hack: depth colour indicates no-tele */
 		p_ptr->redraw |= PR_BPR_WRAITH;
