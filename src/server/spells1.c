b61bffba4270328857aa439500a55088a6b50dc5
C. Blue <mail@c-blue.de>
2025-07-28 01:08:41 +0200

    subinventory: elemental destruction results in drop and not erasure of contents; clarify stealing/destruction.

--- src/server/spells1.c
+++ src/server/spells1.c
@@ -3344,7 +3344,7 @@ int inven_damage(int Ind, inven_func typ, int perc) {
 					}
 
 #ifdef ENABLE_SUBINVEN
- #if 0
+ #if 1
 				/* If we lose a subinventory, remove all items and place them into the player's inventory */
 				if (o_ptr->tval == TV_SUBINVEN && amt >= o_ptr->number) empty_subinven(Ind, i, FALSE, FALSE);
  #else
