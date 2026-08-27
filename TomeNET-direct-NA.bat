1d0a7f364ca42271ee985aa982ad223743b445bd
C. Blue <mail@c-blue.de>
2026-08-27 12:41:00 +0200

    NA server: IP changed (from 68.71.132.101) to 108.165.120.29

--- TomeNET-direct-NA.bat
+++ TomeNET-direct-NA.bat
@@ -2,5 +2,5 @@
 REM Windows batch script to connect directly to the Asia-Pacific server,
 REM without the need for the Meta server or DNS resolution.
 
-TomeNET.exe 68.71.132.101
+TomeNET.exe 108.165.120.29
 REM TomeNET.exe na.tomenet.eu
--- tomenet-direct-NA.sh
+++ tomenet-direct-NA.sh
@@ -1,5 +1,5 @@
 # POSIX shell script to connect directly to the North American server,
 # without the need for the Meta server or DNS resolution.
 
-./tomenet 68.71.132.101
+./tomenet 108.165.120.29
 #./tomenet na.tomenet.eu
