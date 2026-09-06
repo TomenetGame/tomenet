f0d4b53eef4c17f843b28cabd794821ce0d8f8b4
C. Blue <mail@c-blue.de>
2026-09-06 22:59:41 +0200

    Player List subterm window: Correctly update on 'ghost' state change and on any level change.

--- src/server/xtra2.c
+++ src/server/xtra2.c
@@ -5228,10 +5228,9 @@ void check_experience(int Ind) {
 #ifdef TOMENET_WORLDS
 		world_player(p_ptr->id, p_ptr->name, WORLD_INFO(p_ptr), 2, TRUE); // mode 2 -> send WP_UPLAYER
 #endif
+		/* Update his level in everyone's player-list subwindow */
+		Send_playerlist(0, Ind, 2);
 	}
-
-	/* Update his level in everyone's player-list subwindow */
-	if (reglv || newlv) Send_playerlist(0, Ind, 2);
 
 	if (!newlv) {
 		/* Handle stuff */
@@ -10438,7 +10437,6 @@ void player_death(int Ind) {
 
 		/* Remove the death flag */
 		p_ptr->death = FALSE;
-		//ptr->ghost = 0;
 
 		/* Give him his hit points back */
 		p_ptr->chp = p_ptr->mhp;
@@ -10602,7 +10600,6 @@ void player_death(int Ind) {
 		teleport_player(Ind, 200, TRUE);
 		/* Remove the death flag */
 		p_ptr->death = FALSE;
-		//p_ptr->ghost = 0;
 		/* Give him his hit points back */
 		p_ptr->chp = p_ptr->mhp;
 		p_ptr->chp_frac = 0;
@@ -11640,6 +11637,7 @@ void player_death(int Ind) {
 
 	/* Turn him into a ghost */
 	p_ptr->ghost = 1;
+	Send_playerlist(0, Ind, 2);
 #ifdef USE_SOUND_2010
 	handle_music(Ind); //possibly ghostly music!
 #endif
@@ -11983,7 +11981,7 @@ void death_drop_object(player_type *p_ptr, int slot, object_type *o_ptr) {
  */
 void resurrect_player(int Ind, int loss_factor) {
 	player_type *p_ptr = Players[Ind];
-	int reduce;
+	int reduce, oldlv = p_ptr->lev;
 	bool has_exp = p_ptr->max_exp != 0;
 
 	if (!p_ptr->ghost) return;
@@ -12032,6 +12030,8 @@ void resurrect_player(int Ind, int loss_factor) {
 	p_ptr->update |= PU_SANITY;
 	update_stuff(Ind);
 	p_ptr->safe_sane = FALSE;
+	/* If it wasn't updated in check_experience() just now, do it manually - just to remove the red 'ghost' flag colour! */
+	if (oldlv == p_ptr->lev) Send_playerlist(0, Ind, 2);
 
 	/* Message */
 	msg_print(Ind, "\376\377GYou feel life force return to your body!");
